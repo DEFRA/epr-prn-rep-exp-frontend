89f38e3577c96511c29220a706280604164d2225
Shuyad Ali (ext) <shuyad.ali.external@eviden.com>
2025-02-28 15:52:03 +0000

    header and footer for gov uk layout

---
+++ src/Epr.Reprocessor.Exporter.UI.App/Options/FrontEndAccountManagementOptions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Epr.Reprocessor.Exporter.UI.App.Options
+{
+    [ExcludeFromCodeCoverage]
+    public class FrontEndAccountManagementOptions
+    {
+        public const string ConfigSection = "FrontEndAccountManagement";
+
+        public string BaseUrl { get; set; }
+    }
+}
---
+++ src/Epr.Reprocessor.Exporter.UI/ViewComponents/PrimaryNavigationViewComponent.cs
@@ -0,0 +1,55 @@
+﻿using Epr.Reprocessor.Exporter.UI.App.Options;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Epr.Reprocessor.Exporter.UI.ViewModels.Shared;
+
+namespace Epr.Reprocessor.Exporter.UI.ViewComponents
+{
+    public class PrimaryNavigationViewComponent : ViewComponent
+    {
+        private readonly ExternalUrlOptions _externalUrlOptions;
+        private readonly FrontEndAccountManagementOptions _frontEndAccountManagementOptions;
+
+        public PrimaryNavigationViewComponent(
+            IOptions<ExternalUrlOptions> options,
+            IOptions<FrontEndAccountManagementOptions> userAccountManagementOptions)
+        {
+            _frontEndAccountManagementOptions = userAccountManagementOptions.Value;
+            _externalUrlOptions = options.Value;
+        }
+
+        public ViewViewComponentResult Invoke()
+        {
+            var homeLinks = new List<string>
+        {
+            
+        };
+
+            var primaryNavigationModel = new PrimaryNavigationModel();
+            primaryNavigationModel.Items = new List<NavigationModel>();
+
+            //var userData = HttpContext.GetUserData();
+
+            if (userData.Id is not null)
+            {
+                primaryNavigationModel.Items = new List<NavigationModel>
+            {
+                new()
+                {
+                    LinkValue = _externalUrlOptions.LandingPage,
+                    LocalizerKey = "home",
+                    IsActive = homeLinks.Contains(HttpContext.Request.Path.ToString().TrimStart('/'))
+                },
+                new()
+                {
+                    LinkValue = $"{_frontEndAccountManagementOptions.BaseUrl}/",
+                    LocalizerKey = "manage_account_details",
+                }
+            };
+            }
+
+            return View(primaryNavigationModel);
+        }
+    }
+}
