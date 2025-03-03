07c7371bbba148515553fb89276e3baddd264a20
Shuayd Ali <shuyad.ali.external@eviden.com>
2025-03-03 16:19:16 +0000

    514338 - add header and footer partial views for master page (#3)
    * header and footer for gov uk layout
    
    * header and footer for gov uk layout
    
    * merge
    
    * header and footer for gov uk layout

---
+++ src/Epr.Reprocessor.Exporter.UI.App/Options/ExternalUrlOptions.cs
@@ -0,0 +1,16 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Epr.Reprocessor.Exporter.UI.App.Options
+{
+    [ExcludeFromCodeCoverage]
+    public class ExternalUrlOptions
+    {
+        public const string ConfigSection = "ExternalUrls";
+
+        public string LandingPage { get; set; }
+        public string GovUkHome { get; set; }
+        public string PrivacyPage { get; set; }
+        public string AccessibilityPage { get; set; }
+
+    }
+}
---
+++ src/Epr.Reprocessor.Exporter.UI/ViewModels/Shared/NavigationModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Epr.Reprocessor.Exporter.UI.ViewModels.Shared
+{
+    [ExcludeFromCodeCoverage]
+    public class NavigationModel
+    {
+        public string LocalizerKey { get; set; }
+
+        public string LinkValue { get; set; }
+
+        public bool IsActive { get; set; }
+    }
+}
