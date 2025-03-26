cefcae3355fdfad92b3729d4a24c8af29d630537
RobertSmithEviden <199087159+RobertSmithEviden@users.noreply.github.com>
2025-03-26 11:27:11 +0000

    Created the AccreditationController and first placeholder view.

---
+++ src/Epr.Reprocessor.Exporter.UI/Controllers/AccreditationController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Epr.Reprocessor.Exporter.UI.Controllers;
+
+public class AccreditationController : Controller
+{
+    public IActionResult PrnTonnage()
+    {
+        return View();
+    }
+}
