8d281f732722352e0565a08639ee344f554015b3
RobertSmithEviden <199087159+RobertSmithEviden@users.noreply.github.com>
2025-03-27 17:35:49 +0000

    Implementation of UI for PrnTonnage screen. WIP.

---
+++ src/Epr.Reprocessor.Exporter.UI/ViewModels/Accreditation/PrnTonnageViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+using ViewResources = Epr.Reprocessor.Exporter.UI.Resources.Views.Accreditation;
+
+namespace Epr.Reprocessor.Exporter.UI.ViewModels.Accreditation
+{
+    [ExcludeFromCodeCoverage]
+    public class PrnTonnageViewModel
+    {
+        public string MaterialName { get; set; }
+
+        [Required(ErrorMessageResourceName = "error_message", ErrorMessageResourceType = typeof(ViewResources.PrnTonnage))]
+        public string? PrnTonnage { get; set; }
+    }
+}
