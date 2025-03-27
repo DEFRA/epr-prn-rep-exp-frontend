b724f7054af066521223a1824cab6150f166772c
RobertSmithEviden <199087159+RobertSmithEviden@users.noreply.github.com>
2025-03-27 17:35:49 +0000

    Implementation of UI for PrnTonnage screen. WIP.

--- src/Epr.Reprocessor.Exporter.UI/Controllers/AccreditationController.cs
+++ src/Epr.Reprocessor.Exporter.UI/Controllers/AccreditationController.cs
@@ -1,12 +1,31 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using Epr.Reprocessor.Exporter.UI.ViewModels.Accreditation;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Epr.Reprocessor.Exporter.UI.Controllers;
 
 public class AccreditationController : Controller
 {
-    public IActionResult PrnTonnage()
+    [HttpGet]
+    public async Task<IActionResult> PrnTonnage()
     {
-        return View();
+        var viewModel = new PrnTonnageViewModel()
+        {
+            MaterialName = "steel"
+        };
+
+        return View(viewModel);
+    }
+
+    [HttpPost]
+    [IgnoreAntiforgeryToken]
+    public async Task<IActionResult> PrnTonnage(PrnTonnageViewModel viewModel)
+    {
+        if (!ModelState.IsValid)
+        {
+            return View(viewModel);
+        }
+
+        return RedirectToAction("Index");
+
     }
 }
