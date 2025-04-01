6657bc5653e3cebd7c10b233f2df7cc434934284
Shuyad Ali (ext) <shuyad.ali.external@eviden.com>
2025-04-01 22:18:45 +0100

    additional unit tests and refactoring

--- src/Epr.Reprocessor.Exporter.UI/Services/UserJourneySaveAndContinueService.cs
+++ src/Epr.Reprocessor.Exporter.UI/Services/UserJourneySaveAndContinueService.cs
@@ -6,6 +6,7 @@ namespace Epr.Reprocessor.Exporter.UI.Services
     {
         public Task SaveAndContinueAsync(string action, string controller, string data)
         {
+            // add call to facade endpoint to save user journey in the database
             throw new NotImplementedException();
         }
     }
