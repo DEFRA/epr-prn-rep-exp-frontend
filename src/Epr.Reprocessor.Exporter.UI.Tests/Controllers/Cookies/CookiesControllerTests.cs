ff0380f567b0e3facd774c56e1a00f1cb438467f
John Fitzgerald <john.fitzgerald.external@eviden.com>
2025-04-03 15:46:12 +0100

    added unit tests for cookie controller

--- src/Epr.Reprocessor.Exporter.UI.Tests/Controllers/Cookies/CookiesControllerTests.cs
+++ src/Epr.Reprocessor.Exporter.UI.Tests/Controllers/Cookies/CookiesControllerTests.cs
@@ -2,6 +2,7 @@ using Epr.Reprocessor.Exporter.UI.App.Constants;
 using Epr.Reprocessor.Exporter.UI.App.Options;
 using Epr.Reprocessor.Exporter.UI.App.Services.Interfaces;
 using Epr.Reprocessor.Exporter.UI.Controllers.Cookies;
+using Epr.Reprocessor.Exporter.UI.ViewModels;
 using FluentAssertions;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -11,13 +12,14 @@ using Moq;
 using CookieOptions = Epr.Reprocessor.Exporter.UI.App.Options.CookieOptions;
 
 namespace Epr.Reprocessor.Exporter.UI.Tests.Controllers.Cookies;
-
+ 
 [TestClass]
 public class CookiesControllerTests
 {
     ControllerContext _controllerContext;
     Mock<ICookieService> _cookieServiceMock;
-    IOptions<CookieOptions> _CookieOptions;
+    IOptions<CookieOptions> _cookieOptions;
+    IOptions<GoogleAnalyticsOptions> _googleAnalyticsOptions;
 
     [TestInitialize]
     public void TestInitialize()
@@ -32,7 +34,7 @@ public class CookiesControllerTests
 
         _cookieServiceMock = new Mock<ICookieService>();
 
-        _CookieOptions = Options.Create(new CookieOptions
+        _cookieOptions = Options.Create(new CookieOptions
         {
             SessionCookieName = "sessionCookie",
             B2CCookieName = "b2cCookie",
@@ -42,101 +44,99 @@ public class CookiesControllerTests
             TsCookieName = "tsCookie",
             TempDataCookie = "tempDataCookie"
         });
+
+        _googleAnalyticsOptions = Options.Create(new GoogleAnalyticsOptions
+        {
+            CookiePrefix =  "pfx",
+            MeasurementId = "measurementId", 
+        });
     }
 
     [TestMethod]
-    [DataRow(CookieAcceptance.Accept, true)]
-    [DataRow(CookieAcceptance.Reject, false)]
-    public void ShouldUpdateAcceptance(string cookies, bool acceptance)
+    public void Detail_Get_ReturnsViewResultWithCorrectModel()
     {
         // Arrange
-        var cookieService = new Mock<ICookieService>();
+        var returnUrl = "/epr-prn";
+        var cookiesAccepted = true;
+        _cookieServiceMock.Setup(cs => cs.HasUserAcceptedCookies(It.IsAny<IRequestCookieCollection>())).Returns(cookiesAccepted);
 
-        using var cookieController = new CookiesController(cookieService.Object, Mock.Of<IOptions<CookieOptions>>(), Mock.Of<IOptions<GoogleAnalyticsOptions>>())
+        using var controller = new CookiesController(_cookieServiceMock.Object, _cookieOptions, _googleAnalyticsOptions)
+        {
+            ControllerContext = _controllerContext
+        };
+
+        // Act
+        var result = controller.Detail(returnUrl, cookiesAccepted) as ViewResult;
+
+        // Assert
+        result.Should().NotBeNull();
+        result.Model.Should().BeOfType<CookieDetailViewModel>();
+        var model = result.Model as CookieDetailViewModel;
+        model.Should().NotBeNull();
+        model.CookiesAccepted.Should().Be(cookiesAccepted);
+        model.ReturnUrl.Should().Be(returnUrl);
+    }
+
+    [TestMethod]
+    public void Detail_Post_SetsCookieAcceptanceAndReturnsViewResult()
+    {
+        // Arrange
+        var returnUrl = "/epr-prn";
+        var cookiesAccepted = CookieAcceptance.Accept;
+
+        using var controller = new CookiesController(_cookieServiceMock.Object, _cookieOptions, _googleAnalyticsOptions)
         {
             TempData = new TempDataDictionary(_controllerContext.HttpContext, Mock.Of<ITempDataProvider>()),
             ControllerContext = _controllerContext
         };
 
         // Act
-        cookieController.UpdateAcceptance("returnUrl", cookies);
+        var result = controller.Detail(returnUrl, cookiesAccepted) as ViewResult;
 
         // Assert
-        cookieService.Verify(cs => cs.SetCookieAcceptance(acceptance,
-            _controllerContext.HttpContext.Request.Cookies,
-            _controllerContext.HttpContext.Response.Cookies));
+        _cookieServiceMock.Verify(cs => cs.SetCookieAcceptance(true, It.IsAny<IRequestCookieCollection>(), It.IsAny<IResponseCookies>()));
+        result.Should().NotBeNull();
+        result.Model.Should().BeOfType<CookieDetailViewModel>();
     }
 
     [TestMethod]
-    public void ShouldRedirectToReturnUrlWhenUpdateAcceptance()
+    public void UpdateAcceptance_SetsCookieAcceptanceAndRedirects()
     {
         // Arrange
-        const string returnUrl = "returnUrl";
-        var cookieService = new Mock<ICookieService>();
+        var returnUrl = "/epr-prn";
+        var cookies = CookieAcceptance.Accept;
 
-        using var cookieController = new CookiesController(cookieService.Object, Mock.Of<IOptions<CookieOptions>>(), Mock.Of<IOptions<GoogleAnalyticsOptions>>())
+        using var controller = new CookiesController(_cookieServiceMock.Object, _cookieOptions, _googleAnalyticsOptions)
         {
             TempData = new TempDataDictionary(_controllerContext.HttpContext, Mock.Of<ITempDataProvider>()),
-
             ControllerContext = _controllerContext
         };
 
         // Act
-        var result = cookieController.UpdateAcceptance(returnUrl, "cookies");
+        var result = controller.UpdateAcceptance(returnUrl, cookies) as LocalRedirectResult;
 
         // Assert
+        _cookieServiceMock.Verify(cs => cs.SetCookieAcceptance(true, It.IsAny<IRequestCookieCollection>(), It.IsAny<IResponseCookies>()));
+        result.Should().NotBeNull();
         result.Url.Should().Be(returnUrl);
     }
 
     [TestMethod]
-
-    public void ShouldRedirectToHomeWhenUrlNotLocal()
+    public void AcknowledgeAcceptance_RedirectsToReturnUrl()
     {
         // Arrange
-        const string returnUrl = "returnUrl";
+        var returnUrl = "/epr-prn";
 
-        var urlHelper = new Mock<IUrlHelper>();
-        urlHelper.Setup(h => h.IsLocalUrl(returnUrl)).Returns(true);
-
-        using var cookieController = new CookiesController(Mock.Of<ICookieService>(), Mock.Of<IOptions<CookieOptions>>(), Mock.Of<IOptions<GoogleAnalyticsOptions>>())
+        using var controller = new CookiesController(_cookieServiceMock.Object, _cookieOptions, _googleAnalyticsOptions)
         {
-            Url = urlHelper.Object,
-            TempData = new TempDataDictionary(_controllerContext.HttpContext, Mock.Of<ITempDataProvider>()),
             ControllerContext = _controllerContext
         };
 
         // Act
-        var result = cookieController.AcknowledgeAcceptance(returnUrl);
+        var result = controller.AcknowledgeAcceptance(returnUrl) as LocalRedirectResult;
 
         // Assert
+        result.Should().NotBeNull();
         result.Url.Should().Be(returnUrl);
-    }
-
-    [TestMethod]
-    public void AcknowledgeAcceptance_LocalUrl_RedirectsToThatUrl()
-    {
-        // Arrange
-        string localUrl = "/local/path";
-        var urlHelper = new Mock<IUrlHelper>();
-        urlHelper.Setup(u => u.IsLocalUrl(localUrl)).Returns(true);
-        using var cookieController = new CookiesController(_cookieServiceMock.Object, _CookieOptions, Mock.Of<IOptions<GoogleAnalyticsOptions>>())
-        {
-            Url = urlHelper.Object,
-            TempData = new TempDataDictionary(_controllerContext.HttpContext, Mock.Of<ITempDataProvider>()),
-            ControllerContext = _controllerContext
-        };
-
-        // Act
-        var result = cookieController.AcknowledgeAcceptance(localUrl);
-
-        // Assert
-        Assert.IsInstanceOfType(result, typeof(LocalRedirectResult));
-        var redirectResult = (LocalRedirectResult)result;
-        Assert.AreEqual(localUrl, redirectResult.Url);
-    }
-
-    private object ViewBagValue(ViewResult viewResult, string key)
-    {
-        return viewResult.ViewData[key];
     }
 }
