4b405b6696c85d919c8137025f10ebb779e86c91
Venkata Rangala <venkata.rangala.external@eviden.com>
2025-04-08 09:46:32 +0100

    Dev 9 Demo branch

---
+++ src/Epr.Reprocessor.Exporter.UI/Controllers/DemoController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Epr.Reprocessor.Exporter.UI.Controllers
+{
+    [AllowAnonymous]
+    public class DemoController : Controller
+    {
+        public IActionResult Index()
+        {
+			return View();
+		}
+    }
+}
