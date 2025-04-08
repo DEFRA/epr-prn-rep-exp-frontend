2f1babf7c10fb163d9c655db1f4e30316716c02b
Muhammad Ali Shah <muhammad-ali.shah@defra.onmicrosoft.com>
2025-04-08 13:29:48 +0100

    Added resource files

--- src/Epr.Reprocessor.Exporter.UI/Resources/Views/Accreditation/BusinessPlan.Designer.cs
+++ src/Epr.Reprocessor.Exporter.UI/Resources/Views/Accreditation/BusinessPlan.Designer.cs
@@ -61,11 +61,110 @@ namespace Epr.Reprocessor.Exporter.UI.Resources.Views.Accreditation {
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to Communications, including information campaigns.
+        /// </summary>
+        internal static string communications {
+            get {
+                return ResourceManager.GetString("communications", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Developing new markets for products made from recycled packaging waste.
+        /// </summary>
+        internal static string developing_new_markets {
+            get {
+                return ResourceManager.GetString("developing_new_markets", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Developing new uses for recycled waste.
+        /// </summary>
+        internal static string developing_new_uses_for_recycled_waste {
+            get {
+                return ResourceManager.GetString("developing_new_uses_for_recycled_waste", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Business plan.
+        /// </summary>
+        internal static string heading {
+            get {
+                return ResourceManager.GetString("heading", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Enter the percentage of steel PRN income you’ll spend on each item..
+        /// </summary>
+        internal static string lead_para_1 {
+            get {
+                return ResourceManager.GetString("lead_para_1", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to The regulator will expect you to follow this plan. If you later need to change how you spend the income you must explain this in your annual report..
+        /// </summary>
+        internal static string lead_para_2 {
+            get {
+                return ResourceManager.GetString("lead_para_2", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to New reprocessing infrastructure and maintaining existing infrastructure.
+        /// </summary>
+        internal static string new_reprocessing_infrastructure {
+            get {
+                return ResourceManager.GetString("new_reprocessing_infrastructure", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Price support for buying packaging waste or selling recycled packaging waste.
+        /// </summary>
+        internal static string price_support {
+            get {
+                return ResourceManager.GetString("price_support", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Support for business collections.
+        /// </summary>
+        internal static string support_for_business_collections {
+            get {
+                return ResourceManager.GetString("support_for_business_collections", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to Business plan.
         /// </summary>
         internal static string title {
             get {
                 return ResourceManager.GetString("title", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Total Percentage.
+        /// </summary>
+        internal static string total_percentage {
+            get {
+                return ResourceManager.GetString("total_percentage", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Total must equal 100%.
+        /// </summary>
+        internal static string warning_total_must_be_100 {
+            get {
+                return ResourceManager.GetString("warning_total_must_be_100", resourceCulture);
             }
         }
     }
