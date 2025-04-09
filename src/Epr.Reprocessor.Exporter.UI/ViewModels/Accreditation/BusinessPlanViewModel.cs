bbe295382e1f20095b0e64fdb90c473cb32cdc81
Muhammad Ali Shah <muhammad-ali.shah@defra.onmicrosoft.com>
2025-04-09 11:21:56 +0100

    WIP - adding view model

---
+++ src/Epr.Reprocessor.Exporter.UI/ViewModels/Accreditation/BusinessPlanViewModel.cs
@@ -0,0 +1,9 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Epr.Reprocessor.Exporter.UI.ViewModels.Accreditation;
+
+[ExcludeFromCodeCoverage]
+public class BusinessPlanViewModel
+{
+    //0 - 100, whole number
+}
