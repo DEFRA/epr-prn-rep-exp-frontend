4038a4cdc258d5b075e5f37f6c3682a2427a9370
RobertSmithEviden <199087159+RobertSmithEviden@users.noreply.github.com>
2025-04-10 12:24:02 +0100

    Created an HtmlHelper extension to return the application title according to the controller name.

---
+++ src/Epr.Reprocessor.Exporter.UI/Extensions/HtmlHelperExtensions.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Epr.Reprocessor.Exporter.UI.Extensions
+{
+    public static class HtmlHelperExtensions
+    {
+        /// <summary>
+        /// Returns the application title based on the current controller name. Used to correctly populate 
+        /// the text in the site header and browser title, e.g. "Registration", "Accreditation", etc.
+        /// Other logic can be added here if necessary. The returned value is not localised - i.e. pass to 
+        /// a localiser in the view.
+        /// </summary>
+        /// <param name="htmlHelper">The htmlHelper.</param>
+        /// <returns>The non-localised application title.</returns>
+        public static string ApplicationTitle(this IHtmlHelper htmlHelper)
+        {
+            string controllerName = htmlHelper.ViewContext.RouteData.Values["controller"].ToString();
+
+            // Add any required logic.
+            return controllerName switch
+            {
+                "Home" => string.Empty,
+                _ => controllerName
+            };
+        }
+    }
+}
