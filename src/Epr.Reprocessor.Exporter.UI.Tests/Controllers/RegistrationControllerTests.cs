d10071366b514675b8735e554643b37562b01c27
Alex Joyce <150167187+atos-ajoyce@users.noreply.github.com>
2025-04-14 09:49:57 +0100

    Resolve SonarQube and merge issues (#22)
    * add controller, view and view model
    
    * add controller, view and view model
    
    * add controller, view and view model version 3
    
    * add controller, view and view model
    
    * add controller, view and view model
    
    * add controller, view and view model
    
    * add controller, view and view model
    
    * add controller, view and view model
    
    * additional unit tests and refactoring
    
    * additional unit tests and refactoring
    
    * additional unit tests and refactoring
    
    * add middleware to ensure basePath handling
    
    * refactor
    
    * update
    
    * refactoring changes
    
    * refactoring changes
    
    * refactoring changes
    
    * refactoring changes
    
    * refactoring changes
    
    * refactoring changes
    
    * Add sonar analyser packages and comment auth services until enrollment dependency is resolved
    
    * Add serviceprovider unit tests
    
    * Enable authentication
    
    * Remove comments aroung auth code and correct port in launch.json
    
    * Rename unit tests for SonarQube settings
    
    * Update for coverlet.mstest
    
    * Downgrade coverlet version and add sonarqube ignore item group
    
    * Remove pact.net from test project
    
    * Tests - SonarQube report
    
    * Add to SonarQube ignores
    
    * Add sonarqube exclusions
    
    * Exclude controller for now from SQ
    
    * remove duplicate services
    
    ---------
    
    Co-authored-by: Shuyad Ali (ext) <shuyad.ali.external@eviden.com>

--- src/Epr.Reprocessor.Exporter.UI.Tests/Controllers/RegistrationControllerTests.cs
+++ src/Epr.Reprocessor.Exporter.UI.Tests/Controllers/RegistrationControllerTests.cs
@@ -20,6 +20,7 @@ using System.ComponentModel.DataAnnotations;
 using System.Security.Claims;
 using Epr.Reprocessor.Exporter.UI.Services.Interfaces;
 
+
 namespace Epr.Reprocessor.Exporter.UI.Tests.Controllers
 {
     [TestClass]
