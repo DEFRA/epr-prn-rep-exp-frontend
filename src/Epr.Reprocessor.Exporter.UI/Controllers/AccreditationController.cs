30c422970f48171cc992b22b17d28a655f128a48
Paul Cripps (ext) <paul.cripps.external@eviden.com>
2025-04-14 14:57:25 +0100

    Added Accreditation controller plumbing and application saved view

---
+++ src/Epr.Reprocessor.Exporter.UI/Controllers/AccreditationController.cs
@@ -0,0 +1,19 @@
+﻿using Epr.Reprocessor.Exporter.UI.App.Constants;
+using System.Diagnostics.CodeAnalysis;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.FeatureManagement.Mvc;
+
+namespace Epr.Reprocessor.Exporter.UI.Controllers
+{
+    
+    [Route(PagePaths.AccreditationLanding)]
+    [FeatureGate(FeatureFlags.ShowAccreditation)]
+    public class AccreditationController : Controller
+    {
+        [Route(template: PagePaths.ApplicationSaved, Name = PagePaths.ApplicationSaved)]
+        public IActionResult ApplicationSaved()
+        {
+            return View();
+        }
+    }
+}
