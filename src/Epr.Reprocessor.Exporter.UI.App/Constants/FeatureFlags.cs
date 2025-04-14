1d4a01a076a03667f05ef531259ebe08cfdc95bf
Paul Cripps (ext) <paul.cripps.external@eviden.com>
2025-04-14 16:06:41 +0100

    fixed copy paste error before raising PR

--- src/Epr.Reprocessor.Exporter.UI.App/Constants/FeatureFlags.cs
+++ src/Epr.Reprocessor.Exporter.UI.App/Constants/FeatureFlags.cs
@@ -4,5 +4,5 @@ public static class FeatureFlags
 {
     public const string ShowLanguageSwitcher = "ShowLanguageSwitcher";
     public const string ShowRegistration = "ShowRegistration";
-    public const string ShowAccreditation = "ShowRegistration";
+    public const string ShowAccreditation = "ShowAccreditation";
 }
