f81bf9ed74f740561883ae7401134d4a3ab0d3c9
Shuayd Ali <shuyad.ali.external@eviden.com>
2025-04-16 12:21:17 +0100

    #536012 - validation bug fix for grid reference (#46)
    * amend validation for grid reference
    
    * update respurce files
    
    * unit test update

---
+++ src/Epr.Reprocessor.Exporter.UI/Attributes/Validations/MatchAtLeastValidationAttribute.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+using System.Text.RegularExpressions;
+
+namespace Epr.Reprocessor.Exporter.UI.Attributes.Validations
+{
+    [ExcludeFromCodeCoverage]
+    [AttributeUsage(AttributeTargets.Property)]
+    public class MatchAtLeastValidationAttribute : ValidationAttribute
+    {
+        public string RegEx { get; set; } = "{4}";
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            var text = value?.ToString() ?? string.Empty;
+
+           return Regex.IsMatch(text, RegEx, RegexOptions.None, TimeSpan.FromSeconds(250))
+                    ? ValidationResult.Success
+                    : new ValidationResult(ErrorMessage);
+        }
+    }
+}
