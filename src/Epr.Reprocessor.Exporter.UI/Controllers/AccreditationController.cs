2d0273095fe0a40be9cca69d750495149d85cedd
Kris Dyson <kristan.dyson.external@eviden.com>
2025-04-16 13:14:36 +0100

    537455: Bug fixes

--- src/Epr.Reprocessor.Exporter.UI/Controllers/AccreditationController.cs
+++ src/Epr.Reprocessor.Exporter.UI/Controllers/AccreditationController.cs
@@ -2,15 +2,23 @@
 using System.Diagnostics.CodeAnalysis;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.FeatureManagement.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Localization;
 
 namespace Epr.Reprocessor.Exporter.UI.Controllers
 {
     [ExcludeFromCodeCoverage]
     [Route(PagePaths.AccreditationLanding)]
     [FeatureGate(FeatureFlags.ShowAccreditation)]
-    public class AccreditationController : Controller
+    public class AccreditationController(IStringLocalizer<SharedResources> sharedLocalizer) : Controller
     {
         [HttpGet(PagePaths.ApplicationSaved)]
         public IActionResult ApplicationSaved() => View();
+
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            ViewData["ApplicationTitle"] = sharedLocalizer["application_title_accreditation"];
+            base.OnActionExecuting(context);
+        }
     }
 }
