5feb2e5b35382754c13188edb0c311c51b7d333c
Masood Afzal <masood.afzal.external@eviden.com>
2025-04-22 15:30:36 +0100

    feat: #511944 Register site - Select address for notices - FE

--- src/Epr.Reprocessor.Exporter.UI.App/Constants/PagePaths.cs
+++ src/Epr.Reprocessor.Exporter.UI.App/Constants/PagePaths.cs
@@ -16,6 +16,8 @@
         public const string NoAddressFound = "/no-address-found";
         public const string GridReferenceForEnteredReprocessingSite = "/grid-reference-for-entered-reprocessing-site";
         public const string ManualAddressForServiceOfNotices = "/enter-address-for-notices";
+        public const string SelectAddressForServiceOfNotices = "/select-address-for-notices";
+        public const string SelectedAddressForServiceOfNotices = "/selected-address-for-notices";
         public const string CheckYourAnswersForContactDetails = "/check-your-answers-for-contact-details";
 
         public const string TaskList = "reprocessor-registration-task-list";
