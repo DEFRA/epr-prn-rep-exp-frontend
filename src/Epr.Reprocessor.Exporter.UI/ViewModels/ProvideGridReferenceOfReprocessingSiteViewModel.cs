290a05d8919a2eff1b1c61aebb7a56daf3ba2461
Shuayd Ali <shuyad.ali.external@eviden.com>
2025-04-22 15:22:21 +0100

    #543066 - rex registration grid reference validation error message fix (#52)
    * grid reference validation error message fix
    
    * update
    
    * update
    
    * update
    
    * changes

--- src/Epr.Reprocessor.Exporter.UI/ViewModels/ProvideGridReferenceOfReprocessingSiteViewModel.cs
+++ src/Epr.Reprocessor.Exporter.UI/ViewModels/ProvideGridReferenceOfReprocessingSiteViewModel.cs
@@ -1,4 +1,5 @@
-﻿using Epr.Reprocessor.Exporter.UI.Attributes.Validations;
+﻿using Epr.Reprocessor.Exporter.UI.App.Constants;
+using Epr.Reprocessor.Exporter.UI.Attributes.Validations;
 using Epr.Reprocessor.Exporter.UI.Resources.Views.Registration;
 using System.ComponentModel.DataAnnotations;
 
@@ -6,11 +7,11 @@ namespace Epr.Reprocessor.Exporter.UI.ViewModels
 {
     public class ProvideGridReferenceOfReprocessingSiteViewModel
     {
-        [Required(ErrorMessageResourceName = "enter_site_grid_reference_error_message", ErrorMessageResourceType = typeof(ProvideSiteGridReference))]
-        [RegularExpression("\\w*\\d", ErrorMessageResourceName = "grid_reference_must_include_numbers_error_message", ErrorMessageResourceType = typeof(ProvideSiteGridReference))]
-        [MatchAtLeastValidation(RegEx = "\\w*\\d{4}", ErrorMessageResourceName = "grid_reference_with_at_least_4_characters_error_message", ErrorMessageResourceType = typeof(ProvideSiteGridReference))]
-        [MaxNumberValidation(ErrorMessageResourceName = "grid_reference_with_no_more_than_10_characters_error_message", ErrorMessageResourceType = typeof(ProvideSiteGridReference))]
-        [Display(ResourceType = typeof(ProvideSiteGridReference), Name = "enter_site_grid_reference")]
+        [Required(ErrorMessageResourceName = "enter_site_grid_reference_error_message", ErrorMessageResourceType = typeof(ProvideGridReferenceOfReprocessingSite))]
+        [RegularExpression(ValidationRegExConstants.GridReferenceNoSpecialCharacters, ErrorMessageResourceName = "grid_reference_must_include_numbers_error_message", ErrorMessageResourceType = typeof(ProvideSiteGridReference))]
+        [MatchAtLeastValidation(ErrorMessageResourceName = "grid_reference_with_at_least_4_characters_error_message", ErrorMessageResourceType = typeof(ProvideGridReferenceOfReprocessingSite))]
+        [MaxNumberValidation(ErrorMessageResourceName = "grid_reference_with_no_more_than_10_characters_error_message", ErrorMessageResourceType = typeof(ProvideGridReferenceOfReprocessingSite))]
+        [Display(ResourceType = typeof(ProvideGridReferenceOfReprocessingSite), Name = "enter_site_grid_reference")]
         public string GridReference { get; set; }
     }
 }
