6f2e8e8d5bcffafb79691f27761fe9e70177e230
Kris Dyson <kristan.dyson.external@eviden.com>
2025-04-23 09:59:04 +0100

    529900: BusinessPlan url adjustment

--- src/Epr.Reprocessor.Exporter.UI.App/Constants/PagePaths.cs
+++ src/Epr.Reprocessor.Exporter.UI.App/Constants/PagePaths.cs
@@ -28,7 +28,7 @@
         // Accreditation
         public const string SelectPrnTonnage = "prns-plan-to-issue";
         public const string CheckAnswers = "check-your-answers-for-prn-tonnage-and-authority";
-        public const string BusinessPlan = "business-plan";
+        public const string BusinessPlan = "business-plan-percentages";
         public const string SelectAuthority = "select-authority-for-people";
 
     }
