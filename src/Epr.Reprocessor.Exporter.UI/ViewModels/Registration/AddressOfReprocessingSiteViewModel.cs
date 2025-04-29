be74f36599d18ef7b4c3f0fa865f3f571c652bb8
Masood Afzal <masood.afzal.external@eviden.com>
2025-04-30 00:55:33 +0100

    Unit test

--- src/Epr.Reprocessor.Exporter.UI/ViewModels/Registration/AddressOfReprocessingSiteViewModel.cs
+++ src/Epr.Reprocessor.Exporter.UI/ViewModels/Registration/AddressOfReprocessingSiteViewModel.cs
@@ -1,12 +1,15 @@
-﻿using Epr.Reprocessor.Exporter.UI.Enums;
+﻿using System.Diagnostics.CodeAnalysis;
+using Epr.Reprocessor.Exporter.UI.Enums;
 using Epr.Reprocessor.Exporter.UI.ViewModels.Shared;
 
 namespace Epr.Reprocessor.Exporter.UI.ViewModels.Registration;
 
+[ExcludeFromCodeCoverage]
 public class AddressOfReprocessingSiteViewModel
 {
     public AddressOptions? SelectedOption { get; set; }
 
     public AddressViewModel? BusinessAddress { get; set; }
+
     public AddressViewModel? RegisteredAddress { get; set; }
 }
