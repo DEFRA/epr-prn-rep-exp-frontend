888b83f8204c1c113ccf644f4b19ccbf243d39c4
Muhammad Ali Shah <muhammad-ali.shah@defra.onmicrosoft.com>
2025-05-09 14:16:35 +0100

    Added DTOs

---
+++ src/Epr.Reprocessor.Exporter.UI.App/DTOs/Accreditation/AccreditationResquestDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Epr.Reprocessor.Exporter.UI.App.DTOs;
+
+[ExcludeFromCodeCoverage]
+public class AccreditationResponseDto
+{
+    public int? InfrastructurePercentage { get; set; }
+    public int? PackagingWastePercentage { get; set; }
+    public int? BusinessCollectionsPercentage { get; set; }
+    public int? CommunicationsPercentage { get; set; }
+    public int? NewMarketsPercentage { get; set; }
+    public int? NewUsesPercentage { get; set; }
+}
