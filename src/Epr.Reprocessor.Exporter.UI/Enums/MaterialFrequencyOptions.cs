27f2a93668107792d445e7c8e9fda8155c63558b
Your NameShuayd <shuyad.ali.external@eviden.com>
2025-05-12 12:10:43 +0100

    update

---
+++ src/Epr.Reprocessor.Exporter.UI/Enums/MaterialFrequencyOptions.cs
@@ -0,0 +1,18 @@
+﻿using Epr.Reprocessor.Exporter.UI.Resources.Views.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace Epr.Reprocessor.Exporter.UI.Enums
+{
+    public enum MaterialFrequencyOptions
+    {
+        None = 0,
+        [Display(Name = "PerYear", ResourceType = typeof(MaterialFrequencyOptionsResource))]
+        PerYear,
+
+        [Display(Name = "PerMonthly", ResourceType = typeof(MaterialFrequencyOptionsResource))]
+        PerMonthly,
+
+        [Display(Name = "PerWeekly", ResourceType = typeof(MaterialFrequencyOptionsResource))]
+        PerWeekly,
+    }
+}
--- src/Epr.Reprocessor.Exporter.UI/ViewModels/Registration/ProvideWasteManagementLicenseViewModel.cs
+++ src/Epr.Reprocessor.Exporter.UI/ViewModels/Registration/ProvideWasteManagementLicenseViewModel.cs
@@ -1,6 +1,12 @@
-﻿namespace Epr.Reprocessor.Exporter.UI.ViewModels.Registration
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Epr.Reprocessor.Exporter.UI.ViewModels.Registration
 {
     public class ProvideWasteManagementLicenseViewModel
     {
+        [Required]
+        public string Weight { get; set; }
+        [Required]
+        public string SelectedFrequency { get; set; }
     }
 }
