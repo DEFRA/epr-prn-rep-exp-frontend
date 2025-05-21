ecaf6e55f203b9dd77d1799026c02e19cd3a8c27
Paul Cripps (ext) <paul.cripps.external@eviden.com>
2025-05-21 13:56:51 +0100

    Added ApplyingFor2026Accreditation

--- src/Epr.Reprocessor.Exporter.UI.App/Constants/PagePaths.cs
+++ src/Epr.Reprocessor.Exporter.UI.App/Constants/PagePaths.cs
@@ -59,7 +59,8 @@ public class PagePaths
     public const string MoreDetailOnBusinessPlanPRNs = "detail-about-PRN-spend/{accreditationId}";
     public const string MoreDetailOnBusinessPlanPERNs = "detail-about-PERN-spend/{accreditationId}";
     public const string AccreditationSamplingAndInspectionPlan = "sampling-and-inspection-plan";
-        
+    public const string ApplyingFor2026Accreditation = "applying-for-2026-accreditation";
+
     // Use only for pages that have not been developed further than the current page being worked on.
     public const string Placeholder = "/placeholder";
 }
