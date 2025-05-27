90b5afcfc42e8ab7b95e73409c16f50340fbb501
Paul Cripps (ext) <paul.cripps.external@eviden.com>
2025-05-27 09:27:01 +0100

    Merged down from dev

--- src/Epr.Reprocessor.Exporter.UI.App/Constants/PagePaths.cs
+++ src/Epr.Reprocessor.Exporter.UI.App/Constants/PagePaths.cs
@@ -60,7 +60,7 @@ public class PagePaths
     public const string MoreDetailOnBusinessPlanPRNs = "detail-about-PRN-spend/{accreditationId}";
     public const string MoreDetailOnBusinessPlanPERNs = "detail-about-PERN-spend/{accreditationId}";
     public const string AccreditationSamplingAndInspectionPlan = "sampling-and-inspection-plan";
-    public const string ApplyingFor2026Accreditation = "applying-for-2026-accreditation";
+
     public const string AccreditationDeclaration = "declaration/{accreditationId}";
     public const string ApplyingFor2026Accreditation = "applying-for-2026-accreditation/{accreditationId}";
 
