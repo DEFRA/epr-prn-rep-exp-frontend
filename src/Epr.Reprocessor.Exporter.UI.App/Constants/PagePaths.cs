10f2171d03c7957b11198fa901e89ec0e659f920
Hamzah Shakeel <hamzah.shakeel.external@eviden.com>
2025-05-27 09:01:35 +0100

    558985 Fix urls (#177)
    * use proper relative urls
    
    * fix tetss

--- src/Epr.Reprocessor.Exporter.UI.App/Constants/PagePaths.cs
+++ src/Epr.Reprocessor.Exporter.UI.App/Constants/PagePaths.cs
@@ -7,39 +7,39 @@ public class PagePaths
     public const string Cookies = "cookies";
     public const string UpdateCookieAcceptance = "update-cookie-acceptance";
     public const string AcknowledgeCookieAcceptance = "acknowledge-cookie-acceptance";
-    public const string CountryOfReprocessingSite = "/country-of-reprocessing-site";
-    public const string RegistrationLanding = "/registration";
-    public const string AccreditationLanding = "/accreditation";
-    public const string PostcodeOfReprocessingSite = "/postcode-of-reprocessing-site";
-    public const string EnterReprocessingSiteAddress = "/enter-reprocessing-site-address";
-    public const string AddressForLegalDocuments = "/address-for-legal-documents";
-    public const string AddressOfReprocessingSite = "/address-of-reprocessing-site";
-    public const string ApplicationSaved = "/application-saved";
-    public const string NoAddressFound = "/no-address-found";
-    public const string GridReferenceForEnteredReprocessingSite = "/grid-reference-for-entered-reprocessing-site";
-    public const string ManualAddressForServiceOfNotices = "/enter-address-for-notices";
-    public const string SelectAddressForServiceOfNotices = "/select-address-for-notices";
-    public const string SelectedAddressForServiceOfNotices = "/selected-address-for-notices";
-    public const string CheckYourAnswersForContactDetails = "/check-your-answers-for-contact-details";
-    public const string GridReferenceOfReprocessingSite = "/grid-reference-of-reprocessing-site";
-    public const string SelectAddressOfReprocessingSite = "/select-address-of-reprocessing-site";
-    public const string PostcodeForServiceOfNotices = "/postcode-for-notices";
-    public const string ManualAddressForReprocessingSite = "/enter-reprocessing-site-address"; 
-    public const string AddressForNotices = "/address-for-notices";
-    public const string CheckAnswers = "/check-your-answers-for-contact-details";
-    public const string SelectAddressForReprocessingSite = "/select-address-of-reprocessing-site";
-    public const string SelectedAddressForReprocessingSite = "/selected-address-of-reprocessing-site";
-    public const string WastePermitExemptions = "/select-materials-authorised-to-recycle";
-    public const string PpcPermit = "/ppc-permit";
-    public const string InstallationPermit = "/installation-permit";
-    public const string EnvironmentalPermitOrWasteManagementLicence = "/environmental-permit-or-waste-management-licence";
-    public const string ExemptionReferences = "/exemption-references";
+    public const string CountryOfReprocessingSite = "country-of-reprocessing-site";
+    public const string RegistrationLanding = "registration";
+    public const string AccreditationLanding = "accreditation";
+    public const string PostcodeOfReprocessingSite = "postcode-of-reprocessing-site";
+    public const string EnterReprocessingSiteAddress = "enter-reprocessing-site-address";
+    public const string AddressForLegalDocuments = "address-for-legal-documents";
+    public const string AddressOfReprocessingSite = "address-of-reprocessing-site";
+    public const string ApplicationSaved = "application-saved";
+    public const string NoAddressFound = "no-address-found";
+    public const string GridReferenceForEnteredReprocessingSite = "grid-reference-for-entered-reprocessing-site";
+    public const string ManualAddressForServiceOfNotices = "enter-address-for-notices";
+    public const string SelectAddressForServiceOfNotices = "select-address-for-notices";
+    public const string SelectedAddressForServiceOfNotices = "selected-address-for-notices";
+    public const string CheckYourAnswersForContactDetails = "check-your-answers-for-contact-details";
+    public const string GridReferenceOfReprocessingSite = "grid-reference-of-reprocessing-site";
+    public const string SelectAddressOfReprocessingSite = "select-address-of-reprocessing-site";
+    public const string PostcodeForServiceOfNotices = "postcode-for-notices";
+    public const string ManualAddressForReprocessingSite = "enter-reprocessing-site-address"; 
+    public const string AddressForNotices = "address-for-notices";
+    public const string CheckAnswers = "check-your-answers-for-contact-details";
+    public const string SelectAddressForReprocessingSite = "select-address-of-reprocessing-site";
+    public const string SelectedAddressForReprocessingSite = "selected-address-of-reprocessing-site";
+    public const string WastePermitExemptions = "select-materials-authorised-to-recycle";
+    public const string PpcPermit = "ppc-permit";
+    public const string InstallationPermit = "installation-permit";
+    public const string EnvironmentalPermitOrWasteManagementLicence = "environmental-permit-or-waste-management-licence";
+    public const string ExemptionReferences = "exemption-references";
 
-    public const string TaskList = "/reprocessor-registration-task-list";
-    public const string ConfirmNoticesAddress = "/confirm-address-for-notices";
-    public const string PermitForRecycleWaste = "/permit-for-recycling-waste";
-    public const string WasteManagementLicense = "/waste-management-licence";
-    public const string MaximumWeightSiteCanReprocess = "/maximum-weight-the-site-can-reprocess";
+    public const string TaskList = "reprocessor-registration-task-list";
+    public const string ConfirmNoticesAddress = "confirm-address-for-notices";
+    public const string PermitForRecycleWaste = "permit-for-recycling-waste";
+    public const string WasteManagementLicense = "waste-management-licence";
+    public const string MaximumWeightSiteCanReprocess = "maximum-weight-the-site-can-reprocess";
 
     // Accreditation
     public const string EnsureAccreditation = "ensure-accreditation/{materialId}/{applicationTypeId}";
@@ -64,5 +64,5 @@ public class PagePaths
     public const string AccreditationDeclaration = "declaration/{accreditationId}";
 
     // Use only for pages that have not been developed further than the current page being worked on.
-    public const string Placeholder = "/placeholder";
+    public const string Placeholder = "placeholder";
 }
