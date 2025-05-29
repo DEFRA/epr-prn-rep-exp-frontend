8ff218582229648386a911880d8b79d08d950cfa
Masood Afzal <masood.afzal.external@eviden.com>
2025-05-29 17:44:37 +0100

    Minor changes

--- src/Epr.Reprocessor.Exporter.UI/Controllers/Registration/RegistrationController.Main.cs
+++ src/Epr.Reprocessor.Exporter.UI/Controllers/Registration/RegistrationController.Main.cs
@@ -234,36 +234,6 @@ namespace Epr.Reprocessor.Exporter.UI.Controllers
             return View(model);
         }
 
-        [HttpGet]
-        [Route(PagePaths.GridReferenceForEnteredReprocessingSite)]
-        public async Task<IActionResult> ProvideSiteGridReference()
-        {
-            var model = new ProvideSiteGridReferenceViewModel();
-            var session = await _sessionManager.GetSessionAsync(HttpContext.Session) ?? new ReprocessorExporterRegistrationSession();
-            session.Journey = new List<string> { PagePaths.RegistrationLanding, PagePaths.GridReferenceForEnteredReprocessingSite };
-
-            session.RegistrationApplicationSession.ReprocessingSite!.SetSourcePage(PagePaths.GridReferenceForEnteredReprocessingSite);
-            await SaveSession(session, PagePaths.GridReferenceForEnteredReprocessingSite, PagePaths.RegistrationLanding);
-
-            SetTempBackLink(PagePaths.AddressOfReprocessingSite, PagePaths.GridReferenceForEnteredReprocessingSite);
-
-            return View(model);
-        }
-
-        [HttpPost]
-        [Route(PagePaths.GridReferenceForEnteredReprocessingSite)]
-        public async Task<IActionResult> ProvideSiteGridReference(ProvideSiteGridReferenceViewModel model, string buttonAction)
-        {
-            SetTempBackLink(PagePaths.AddressOfReprocessingSite, PagePaths.GridReferenceForEnteredReprocessingSite);
-
-            if (!ModelState.IsValid)
-            {
-                return View(model);
-            }
-
-            return ReturnSaveAndContinueRedirect(buttonAction, "/", PagePaths.ApplicationSaved);
-        }
-
         [HttpGet($"{PagePaths.RegistrationLanding}{PagePaths.ApplicationSaved}", Name = RegistrationRouteIds.ApplicationSaved)]
         public IActionResult ApplicationSaved() => View();
 
--- src/Epr.Reprocessor.Exporter.UI/Controllers/Registration/RegistrationController.ReprocessingSite.GridReference .cs
+++ src/Epr.Reprocessor.Exporter.UI/Controllers/Registration/RegistrationController.ReprocessingSite.GridReference .cs
@@ -1,14 +1,40 @@
-﻿using System.Diagnostics.CodeAnalysis;
+﻿using Epr.Reprocessor.Exporter.UI.App.Constants;
+using Epr.Reprocessor.Exporter.UI.Sessions;
+using Epr.Reprocessor.Exporter.UI.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Epr.Reprocessor.Exporter.UI.Controllers;
 
 public partial class RegistrationController
 {
-    [ExcludeFromCodeCoverage]
+
     [HttpGet]
-    public async Task<IActionResult> Test()
+    [Route(PagePaths.GridReferenceForEnteredReprocessingSite)]
+    public async Task<IActionResult> ProvideSiteGridReference()
     {
-        return View();
+        var model = new ProvideSiteGridReferenceViewModel();
+        var session = await _sessionManager.GetSessionAsync(HttpContext.Session) ?? new ReprocessorExporterRegistrationSession();
+        session.Journey = new List<string> { PagePaths.RegistrationLanding, PagePaths.GridReferenceForEnteredReprocessingSite };
+
+        session.RegistrationApplicationSession.ReprocessingSite!.SetSourcePage(PagePaths.GridReferenceForEnteredReprocessingSite);
+        await SaveSession(session, PagePaths.GridReferenceForEnteredReprocessingSite, PagePaths.RegistrationLanding);
+
+        SetTempBackLink(PagePaths.AddressOfReprocessingSite, PagePaths.GridReferenceForEnteredReprocessingSite);
+
+        return View(model);
+    }
+
+    [HttpPost]
+    [Route(PagePaths.GridReferenceForEnteredReprocessingSite)]
+    public async Task<IActionResult> ProvideSiteGridReference(ProvideSiteGridReferenceViewModel model, string buttonAction)
+    {
+        SetTempBackLink(PagePaths.AddressOfReprocessingSite, PagePaths.GridReferenceForEnteredReprocessingSite);
+
+        if (!ModelState.IsValid)
+        {
+            return View(model);
+        }
+
+        return ReturnSaveAndContinueRedirect(buttonAction, "/", PagePaths.ApplicationSaved);
     }
 }
