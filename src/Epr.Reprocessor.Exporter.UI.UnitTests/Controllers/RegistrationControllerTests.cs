164bd95ff2b10bbef86796c7928eca1512a419b5
Masood Afzal <masood.afzal.external@eviden.com>
2025-05-30 09:24:56 +0100

    Fixed broken unit test

--- src/Epr.Reprocessor.Exporter.UI.UnitTests/Controllers/RegistrationControllerTests.cs
+++ src/Epr.Reprocessor.Exporter.UI.UnitTests/Controllers/RegistrationControllerTests.cs
@@ -1134,7 +1134,7 @@ public class RegistrationControllerTests
     }
 
     [TestMethod]
-    [DataRow("SaveAndContinue", "/")]
+    [DataRow("SaveAndContinue", PagePaths.AddressForNotices)]
     [DataRow("SaveAndComeBackLater", PagePaths.ApplicationSaved)]
     public async Task ProvideSiteGridReference_OnSubmit_ShouldRedirect(string actionButton, string expectedReturnUrl)
     {
