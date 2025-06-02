146c6edf40a517550fe622d945d653e8d3cbfe14
Robert Smith <199087159+RobertSmithEviden@users.noreply.github.com>
2025-06-02 16:39:36 +0100

    562812: Fix to accreditation declaration screen URL. (#219)

--- src/Epr.Reprocessor.Exporter.UI.App/Constants/PagePaths.cs
+++ src/Epr.Reprocessor.Exporter.UI.App/Constants/PagePaths.cs
@@ -60,8 +60,7 @@ public class PagePaths
     public const string MoreDetailOnBusinessPlanPRNs = "detail-about-PRN-spend/{accreditationId}";
     public const string MoreDetailOnBusinessPlanPERNs = "detail-about-PERN-spend/{accreditationId}";
     public const string AccreditationSamplingAndInspectionPlan = "sampling-and-inspection-plan";
-
-    public const string AccreditationDeclaration = "declaration/{accreditationId}";
+    public const string AccreditationDeclaration = "accreditation-application-declaration/{accreditationId}";
     public const string ReprocessorAccreditationSamplingFileUpload = "reprocessor-accreditation-sampling-file-upload";
     public const string ExporterAccreditationSamplingFileUpload = "exporter-accreditation-sampling-file-upload";
     public const string ApplyingFor2026Accreditation = "applying-for-2026-accreditation/{accreditationId}";
