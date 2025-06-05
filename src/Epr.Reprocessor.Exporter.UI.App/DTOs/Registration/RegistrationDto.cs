990543ca483eccbf00150c7629c2041c8a86e8a5
Ravi Sharma Gautam <ravi.sharmagautam@defradev.onmicrosoft.com>
2025-06-05 13:38:57 +0100

    excluded dto from code coverage

--- src/Epr.Reprocessor.Exporter.UI.App/DTOs/Registration/RegistrationDto.cs
+++ src/Epr.Reprocessor.Exporter.UI.App/DTOs/Registration/RegistrationDto.cs
@@ -1,8 +1,10 @@
 ﻿using Epr.Reprocessor.Exporter.UI.App.Enums.Accreditation;
 using Epr.Reprocessor.Exporter.UI.App.Enums.Registration;
+using System.Diagnostics.CodeAnalysis;
 
 namespace Epr.Reprocessor.Exporter.UI.App.DTOs.Registration;
 
+[ExcludeFromCodeCoverage(Justification = "Its a DTO and doesn't have  any logic")]
 public class RegistrationDto
 {
     public Guid RegistrationId { get; set; }
