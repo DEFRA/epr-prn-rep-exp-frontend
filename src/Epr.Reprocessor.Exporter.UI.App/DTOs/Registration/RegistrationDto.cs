8846d64d8f3c2e71dce94888c7fa46ee4d794e13
Ravi Sharma Gautam <ravi.sharmagautam@defradev.onmicrosoft.com>
2025-06-05 12:23:44 +0100

    intial dto for dashboard

---
+++ src/Epr.Reprocessor.Exporter.UI.App/DTOs/Registration/RegistrationDto.cs
@@ -0,0 +1,19 @@
+﻿using Epr.Reprocessor.Exporter.UI.App.Enums.Accreditation;
+using Epr.Reprocessor.Exporter.UI.App.Enums.Registration;
+
+namespace Epr.Reprocessor.Exporter.UI.App.DTOs.Registration;
+
+public class RegistrationDto
+{
+    public Guid RegistrationId { get; set; }
+    public int RegistrationMaterialId { get; set; }
+    public int MaterialId { get; set; }
+    public string Material { get; set; }
+    public int ApplicationTypeId { get; set; }
+    public string ApplicationType { get; set; }
+    public RegistrationStatus RegistrationStatus { get; set; }
+    public AccreditationStatus AccreditationStatus { get; set; }
+    public int? ReprocessingSiteId { get; set; }
+    public AddressDto? ReprocessingSiteAddress { get; set; }
+    public int Year { get; set; }
+}
