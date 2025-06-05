7a5b597cf4e59d2b6e7178f13fb39b92fd1b2318
Ravi Sharma Gautam <66359108+Ravi-SharmaGautam@users.noreply.github.com>
2025-06-05 14:43:25 +0100

    intial dto for dashboard (#244)
    * intial dto for dashboard
    
    * excluded dto from code coverage
    
    ---------
    
    Co-authored-by: Ravi Sharma Gautam <ravi.sharmagautam@defradev.onmicrosoft.com>

---
+++ src/Epr.Reprocessor.Exporter.UI.App/DTOs/Registration/RegistrationDto.cs
@@ -0,0 +1,21 @@
+﻿using Epr.Reprocessor.Exporter.UI.App.Enums.Accreditation;
+using Epr.Reprocessor.Exporter.UI.App.Enums.Registration;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Epr.Reprocessor.Exporter.UI.App.DTOs.Registration;
+
+[ExcludeFromCodeCoverage(Justification = "Its a DTO and doesn't have  any logic")]
+public class RegistrationDto
+{
+    public Guid RegistrationId { get; set; }
+    public int RegistrationMaterialId { get; set; }
+    public int MaterialId { get; set; }
+    public string Material { get; set; }
+    public int ApplicationTypeId { get; set; }
+    public string ApplicationType { get; set; }
+    public RegistrationStatus RegistrationStatus { get; set; }
+    public AccreditationStatus AccreditationStatus { get; set; }
+    public int? ReprocessingSiteId { get; set; }
+    public AddressDto? ReprocessingSiteAddress { get; set; }
+    public int Year { get; set; }
+}
