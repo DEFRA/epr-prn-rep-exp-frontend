3aabbd0d1ca763a7dea4572b303c9d06ee4a7c81
John Fitzgerald <john.fitzgerald.external@eviden.com>
2025-06-12 14:19:39 +0100

    added end point

--- src/Epr.Reprocessor.Exporter.UI.App/Constants/Endpoints.cs
+++ src/Epr.Reprocessor.Exporter.UI.App/Constants/Endpoints.cs
@@ -5,4 +5,5 @@ public static class Endpoints
     public const string CreateRegistration = "api/v1/Registrations";
     public const string UpdateRegistrationTaskStatus = "api/v1/Registrations/{registrationId}/TaskStatus";
     public const string UpdateRegistrationSiteAddress = "api/v1/Registrations/{registrationId}/SiteAddress";
+    public const string RegistrationOverview = "api/v1/Registrations/{registrationId}/RegistrationOverview";
 }
