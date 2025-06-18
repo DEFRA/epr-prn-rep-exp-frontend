1f719ad7454cf0d671c8f7e5e5fac28af11d410e
Masood Afzal <masood.afzal.external@eviden.com>
2025-06-18 09:26:39 +0100

    Minor code refactor

--- src/Epr.Reprocessor.Exporter.UI.App/DTOs/Registration/MaterialsPermitTypeDto.cs
+++ src/Epr.Reprocessor.Exporter.UI.App/DTOs/Registration/MaterialsPermitTypeDto.cs
@@ -1,5 +1,8 @@
-﻿namespace Epr.Reprocessor.Exporter.UI.App.DTOs.Registration;
+﻿using Epr.Reprocessor.Exporter.UI.App.Enums.Registration;
+
+namespace Epr.Reprocessor.Exporter.UI.App.DTOs.Registration;
 
 public class MaterialsPermitTypeDto : IdNamePairDto
 {
+    public bool HasPermitNumber => ((MaterialPermitType)Id != MaterialPermitType.WasteExemption && (MaterialPermitType)Id != MaterialPermitType.None);
 }
