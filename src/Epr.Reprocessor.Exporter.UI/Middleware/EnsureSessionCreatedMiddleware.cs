011084940f0ea8e71ef2062b1c3004f6f1e98b33
krisdys-eviden <kristan.dyson.external@eviden.com>
2025-06-23 15:30:17 +0100

    Merge to main (#301)
    * Merge to main
    
    * fixing SQ issues
    
    * fixed final one
    
    ---------
    
    Co-authored-by: hamzahshakeel907 <hamzah.shakeel.external@eviden.com>

---
+++ src/Epr.Reprocessor.Exporter.UI/Middleware/EnsureSessionCreatedMiddleware.cs
@@ -0,0 +1,31 @@
+﻿namespace Epr.Reprocessor.Exporter.UI.Middleware;
+
+/// <summary>
+/// Middleware to ensure that a session of type <see cref="ReprocessorRegistrationSession"/> is created.
+/// </summary>
+/// <param name="next">A delegate to the next middleware in the pipeline.</param>
+[ExcludeFromCodeCoverage]
+public class EnsureSessionCreatedMiddleware(RequestDelegate next)
+{
+    public async Task InvokeAsync(
+        HttpContext context,
+        ISessionManager<ReprocessorRegistrationSession> sessionManager,
+        ILogger<EnsureSessionCreatedMiddleware> logger)
+    {
+        // Ensure the session is created
+        if (context.Session.IsAvailable && context.User.Identity is not null && context.User.Identity.IsAuthenticated)
+        {
+            logger.LogInformation($"No session of type {nameof(ReprocessorRegistrationSession)}, creating.");
+
+            var session = await sessionManager.GetSessionAsync(context.Session);
+
+            if (session is null)
+            {
+                await sessionManager.SaveSessionAsync(context.Session, new ReprocessorRegistrationSession());
+            }
+        }
+
+        // Call the next middleware in the pipeline
+        await next(context);
+    }
+}
---
+++ src/Epr.Reprocessor.Exporter.UI/ViewModels/AddOrganisationViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Epr.Reprocessor.Exporter.UI.ViewModels
+{
+    public class AddOrganisationViewModel
+    {
+        public string FullName => $"{FirstName} {LastName}";
+        public string AddOrganisationLink { get; set; }
+        public string ReadMoreAboutApprovedPersonLink { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
