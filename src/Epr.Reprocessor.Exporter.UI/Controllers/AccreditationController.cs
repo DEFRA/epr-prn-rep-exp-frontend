a27f427257f0cdd06ea4d3637478b5e8e6e3e8e2
Dat Tonthat (ext) <dat.tonthat.external@atos.net>
2025-06-25 15:35:35 +0100

    1.Added resources, view-model & view EvidenceOfEquivalentStandardsCheckSiteFulfillsConditions.cshtml 2.Added actions AccreditationController.EvidenceOfEquivalentStandardsCheckSiteFulfillsConditions() 3.Added class FulfilmentsOfWasteProcessingConditions.cs

--- src/Epr.Reprocessor.Exporter.UI/Controllers/AccreditationController.cs
+++ src/Epr.Reprocessor.Exporter.UI/Controllers/AccreditationController.cs
@@ -819,6 +819,42 @@ namespace Epr.Reprocessor.Exporter.UI.Controllers
             return View(model);
         }
 
+        [HttpGet(PagePaths.EvidenceOfEquivalentStandardsCheckSiteFulfillsConditions)]
+        public async Task<IActionResult> EvidenceOfEquivalentStandardsCheckSiteFulfillsConditions(
+                                         string orgName, string addrLine1, string addrLine2, string addrLine3)
+        {
+            var model = new EvidenceOfEquivalentStandardsCheckSiteFulfillsConditionsViewModel
+            {
+                OverseasSite = new OverseasReprocessingSite
+                {
+                    OrganisationName = orgName, AddressLine1 = addrLine1, AddressLine2 = addrLine2, AddressLine3 = addrLine3
+                }
+            };
+
+            return View(model);
+        }
+
+        [HttpPost(PagePaths.EvidenceOfEquivalentStandardsCheckSiteFulfillsConditions)]
+        public async Task<IActionResult> EvidenceOfEquivalentStandardsCheckSiteFulfillsConditions(EvidenceOfEquivalentStandardsCheckSiteFulfillsConditionsViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            model.SiteFulfillsAllConditions = model.SelectedOption is FulfilmentsOfWasteProcessingConditions.ConditionsFulfilledEvidenceUploadUnwanted
+                                              or FulfilmentsOfWasteProcessingConditions.ConditionsFulfilledEvidenceUploadwanted;
+
+            if (model.SelectedOption is FulfilmentsOfWasteProcessingConditions.ConditionsFulfilledEvidenceUploadUnwanted)
+            {
+                var site = model.OverseasSite;
+                return RedirectToAction(nameof(EvidenceOfEquivalentStandardsCheckYourAnswers),
+                       new { orgName = site.OrganisationName, addrLine1 = site.AddressLine1, addrLine2 = site.AddressLine2,
+                             addrLine3 = site.AddressLine3, conditionsFulfilled = true });
+            }
+
+            return View(model);
+        }
+
         private AccreditationRequestDto GetAccreditationRequestDto(AccreditationDto accreditation)
         {
             return new AccreditationRequestDto
