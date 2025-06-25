2587a1950653e9c0e22802410514a93fc99441e0
Mark Forster <160658973+MW-Forster@users.noreply.github.com>
2025-06-25 10:24:46 +0100

    Added Overseas Address Session (#315)
    * Added Overseas Address Session
    
    * Removed WhiteSpace
    
    * Updates for Dto/Domain Models
    
    * PR Comment Updates
    
    * Updated Controller
    
    * Fix
    
    * Removed Exporter Controller

---
+++ src/Epr.Reprocessor.Exporter.UI.App/Domain/Exporter/OverseasAddress.cs
@@ -0,0 +1,10 @@
+﻿using Epr.Reprocessor.Exporter.UI.App.DTOs.Registration.Exporter;
+
+namespace Epr.Reprocessor.Exporter.UI.App.Domain.Registration.Exporter;
+
+[ExcludeFromCodeCoverage]
+public class OverseasAddress : OverseasAddressDto
+{
+    public bool IsActive { get; set; }
+
+}
