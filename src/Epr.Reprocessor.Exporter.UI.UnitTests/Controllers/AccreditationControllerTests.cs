b02fa2f76c77b60103c837545fef3dacb222d777
DavidTonthat <104467315+davidtonthat@users.noreply.github.com>
2025-06-25 10:21:08 +0100

    1.Added resource & view-model for view EvidenceOfEquivalentStandardsCheckYourAnswers.cshtml 2.Added action AccreditationController.EvidenceOfEquivalentStandardsCheckYourAnswers() 3.Updated DTO OverseasReprocessingSite.cs (#314)
    Co-authored-by: Dat Tonthat (ext) <dat.tonthat.external@atos.net>

--- src/Epr.Reprocessor.Exporter.UI.UnitTests/Controllers/AccreditationControllerTests.cs
+++ src/Epr.Reprocessor.Exporter.UI.UnitTests/Controllers/AccreditationControllerTests.cs
@@ -2162,7 +2162,6 @@ namespace Epr.Reprocessor.Exporter.UI.UnitTests.Controllers
         #endregion
 
         #region UploadEvidenceOfEquivalentStandards
-
         [TestMethod]
         public async Task UploadEvidenceOfEquivalentStandards_ReturnsViewWithModel()
         {
@@ -2174,7 +2173,7 @@ namespace Epr.Reprocessor.Exporter.UI.UnitTests.Controllers
                 MaterialName = "Glass"
             };
             List<OverseasReprocessingSite> overseasSites = [
-                new() { OrganisationName = "Hun Manet Recycler Ltd", Address = "Tuol Sleng Road, Battambang, Cambodia"}
+                new() { OrganisationName = "Hun Manet Recycler Ltd", AddressLine1 = "Tuol Sleng Road", AddressLine2 = "Battambang", AddressLine3 = "Cambodia"}
             ];
 ;
             _mockAccreditationService.Setup(s => s.GetAccreditation(accreditationId)).ReturnsAsync(accreditation);
@@ -2191,6 +2190,29 @@ namespace Epr.Reprocessor.Exporter.UI.UnitTests.Controllers
             model.Should().NotBeNull();
             model.MaterialName.Should().Be(accreditation.MaterialName);
             model.OverseasSites.Should().BeEquivalentTo(overseasSites);
+        }
+        #endregion
+
+        #region EvidenceOfEquivalentStandardsCheckYourAnswers
+        [TestMethod]
+        public async Task EvidenceOfEquivalentStandardsCheckYourAnswers_ReturnsViewWithModel()
+        {
+            // Arrange
+            OverseasReprocessingSite overseasSite = new()
+            {
+                OrganisationName = "Hun Manet Recycler Ltd", AddressLine1 = "Tuol Sleng Road", AddressLine2 = "Battambang", AddressLine3 = "Cambodia"
+            };
+
+            // Act
+            var result = await _controller.EvidenceOfEquivalentStandardsCheckYourAnswers(
+                                overseasSite.OrganisationName, overseasSite.AddressLine1, overseasSite.AddressLine2, overseasSite.AddressLine3);
+
+            // Assert
+            var viewResult = result as ViewResult;
+            var model = viewResult.Model as EvidenceOfEquivalentStandardsCheckYourAnswersViewModel;
+            viewResult.Should().NotBeNull();
+            model.Should().NotBeNull();
+            model.OverseasSite.Should().BeEquivalentTo(overseasSite);
         }
         #endregion
     }
