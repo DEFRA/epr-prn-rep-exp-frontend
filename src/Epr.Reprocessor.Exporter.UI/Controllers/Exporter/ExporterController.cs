d45fd3596844dc6aa082d1ef8849c5917049e9da
mw-forster <mark.forster@eviden.com>
2025-06-25 09:39:43 +0100

    Updated Controller

---
+++ src/Epr.Reprocessor.Exporter.UI/Controllers/Exporter/ExporterController.cs
@@ -0,0 +1,10 @@
+﻿using Epr.Reprocessor.Exporter.UI.Mapper;
+using Epr.Reprocessor.Exporter.UI.ViewModels.Registration.Exporter;
+
+namespace Epr.Reprocessor.Exporter.UI.Controllers;
+
+[FeatureGate(FeatureFlags.ShowRegistration)]
+[Route(PagePaths.RegistrationLanding)]
+public class ExporterController() : Controller
+{
+}
