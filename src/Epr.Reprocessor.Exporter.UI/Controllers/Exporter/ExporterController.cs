1c90fbfd9da2aa3af914652002021e8e6b3c03da
mw-forster <mark.forster@eviden.com>
2025-06-25 09:44:34 +0100

    Fix

--- src/Epr.Reprocessor.Exporter.UI/Controllers/Exporter/ExporterController.cs
+++ src/Epr.Reprocessor.Exporter.UI/Controllers/Exporter/ExporterController.cs
@@ -1,7 +1,4 @@
-﻿using Epr.Reprocessor.Exporter.UI.Mapper;
-using Epr.Reprocessor.Exporter.UI.ViewModels.Registration.Exporter;
-
-namespace Epr.Reprocessor.Exporter.UI.Controllers;
+﻿namespace Epr.Reprocessor.Exporter.UI.Controllers;
 
 [FeatureGate(FeatureFlags.ShowRegistration)]
 [Route(PagePaths.RegistrationLanding)]
