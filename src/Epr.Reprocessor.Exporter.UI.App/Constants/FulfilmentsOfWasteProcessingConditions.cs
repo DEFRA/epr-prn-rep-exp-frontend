ac0ab39f99724346daba6ae8ecaba7fd417903de
DavidTonthat <104467315+davidtonthat@users.noreply.github.com>
2025-06-26 15:41:02 +0100

    1.Added resources, view-model & view EvidenceOfEquivalentStandardsCheckSiteFulfillsConditions.cshtml 2.Added actions AccreditationController.EvidenceOfEquivalentStandardsCheckSiteFulfillsConditions() 3.Added class FulfilmentsOfWasteProcessingConditions.cs (#321)
    Co-authored-by: Dat Tonthat (ext) <dat.tonthat.external@atos.net>

---
+++ src/Epr.Reprocessor.Exporter.UI.App/Constants/FulfilmentsOfWasteProcessingConditions.cs
@@ -0,0 +1,8 @@
+﻿namespace Epr.Reprocessor.Exporter.UI.App.Constants;
+
+public static class FulfilmentsOfWasteProcessingConditions
+{
+    public const string ConditionsFulfilledEvidenceUploadUnwanted = "All conditions fulfilled, evidence upload not wanted";
+    public const string ConditionsFulfilledEvidenceUploadwanted   = "All conditions fulfilled but evidence upload wanted";
+    public const string AllConditionsNotFulfilled = "All conditions not fulfilled";
+}
