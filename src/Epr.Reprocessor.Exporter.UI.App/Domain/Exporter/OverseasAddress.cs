72bfd7bb101cce334b2f991148d0a251af56e708
mw-forster <mark.forster@eviden.com>
2025-06-26 13:15:06 +0100

    fixed inheritance

--- src/Epr.Reprocessor.Exporter.UI.App/Domain/Exporter/OverseasAddress.cs
+++ src/Epr.Reprocessor.Exporter.UI.App/Domain/Exporter/OverseasAddress.cs
@@ -1,10 +1,29 @@
 ﻿using Epr.Reprocessor.Exporter.UI.App.DTOs.Registration.Exporter;
+using System.ComponentModel.DataAnnotations;
 
 namespace Epr.Reprocessor.Exporter.UI.App.Domain.Registration.Exporter;
 
 [ExcludeFromCodeCoverage]
-public class OverseasAddress : OverseasAddressDto
+public class OverseasAddress
 {
     public bool IsActive { get; set; }
+    public Guid Id { get; set; }
+    public required string OrganisationName { get; set; }
+    public required string Country { get; set; }
+    [MaxLength(100)]
+    public required string AddressLine1 { get; set; }
+    [MaxLength(100)]
+    public required string AddressLine2 { get; set; }
+    [MaxLength(70)]
+    public required string CityorTown { get; set; }
+    [MaxLength(70)]
+    public required string StateProvince { get; set; }
+    [MaxLength(20)]
+    public required string PostCode { get; set; }
+    [MaxLength(100)]
+    public required string SiteCoordinates { get; set; }
+
+    public List<OverseasAddressContact> OverseasAddressContact { get; set; } = new();
+    public List<OverseasAddressWasteCodes> OverseasAddressWasteCodes { get; set; } = new();
 
 }
