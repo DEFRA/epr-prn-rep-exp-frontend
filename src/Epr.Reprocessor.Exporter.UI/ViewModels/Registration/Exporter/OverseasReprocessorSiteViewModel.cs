885665b0be8e4a829e33adacf569d0aef77297c4
mw-forster <mark.forster@eviden.com>
2025-06-29 00:16:10 +0100

    Updated Text and validation

--- src/Epr.Reprocessor.Exporter.UI/ViewModels/Registration/Exporter/OverseasReprocessorSiteViewModel.cs
+++ src/Epr.Reprocessor.Exporter.UI/ViewModels/Registration/Exporter/OverseasReprocessorSiteViewModel.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc.ModelBinding;
 using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
 using System.ComponentModel.DataAnnotations;
+using System.Runtime.InteropServices;
 
 namespace Epr.Reprocessor.Exporter.UI.ViewModels.Registration.Exporter
 {
@@ -12,34 +13,37 @@ namespace Epr.Reprocessor.Exporter.UI.ViewModels.Registration.Exporter
         [BindNever, ValidateNever]
         public IEnumerable<string> Countries { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Select the country the site is in")]
         public string Country { get; set; }
 
-        [Required]
+        [Required(ErrorMessage="Enter the organisation’s name")]
         public string OrganisationName { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Enter address line 1, typically the building and street")]
         [MaxLength(100)]
         public string AddressLine1 { get; set; }
         [MaxLength(100)]
-        public string AddressLine2 { get; set; }
+        public string? AddressLine2 { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Enter a city or town")]
         [MaxLength(70)]
         public string CityorTown { get; set; }
         [MaxLength(70)]
-        public string StateProvince { get; set; }
+        public string? StateProvince { get; set; }
         [MaxLength(20)]
-        public string Postcode { get; set; }
+        public string? Postcode { get; set; }
+        [Required(ErrorMessage = "Enter the latitude and longitude coordinates for the site’s main entrance")]
         [MaxLength(100)]
         public string SiteCoordinates { get; set; }
-
-        [Required]
+        [Required(ErrorMessage = "Enter the name of the person the regulator can contact")]
+        [MaxLength(100)]
         public string ContactFullName { get; set; }
-
-        [EmailAddress]
+        [Required(ErrorMessage = "Enter the email of the person the regulator can contact")]
+        [EmailAddress(ErrorMessage = "Enter an email address in the correct format, like name@example.com")]
+        [MaxLength(100)]
         public string Email { get; set; }
-
+        [Required(ErrorMessage = "Enter the phone number of the person the regulator can contact")]
+        [MaxLength(25)]
         public string PhoneNumber { get; set; }
     }
 }
