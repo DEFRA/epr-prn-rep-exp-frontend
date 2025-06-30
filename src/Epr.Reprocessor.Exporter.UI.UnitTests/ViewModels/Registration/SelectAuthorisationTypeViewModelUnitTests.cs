984fd168aa6388bb159b7b7fb72ef50a99875aee
hamzahshakeel907 <hamzah.shakeel.external@eviden.com>
2025-06-30 09:38:45 +0100

    sorted tests

--- src/Epr.Reprocessor.Exporter.UI.UnitTests/ViewModels/Registration/SelectAuthorisationTypeViewModelUnitTests.cs
+++ src/Epr.Reprocessor.Exporter.UI.UnitTests/ViewModels/Registration/SelectAuthorisationTypeViewModelUnitTests.cs
@@ -4,81 +4,12 @@
 public class SelectAuthorisationTypeViewModelUnitTests
 {
     [TestMethod]
-    public void SelectAuthorisationTypeViewModel_ValidModel_WasteExemption_NoPermitNumberNecessary_ButProvidePermitToEnsureSuccessfulValidation()
+    public void SelectAuthorisationTypeViewModel_InvalidModel_RequiredFieldNotProvided()
     {
         // Arrange
         var model = new SelectAuthorisationTypeViewModel
         {
-            SelectedAuthorisation = 1,
-            AuthorisationTypes =
-            [
-                new()
-                {
-                    Id = 1,
-                    Label = "label",
-                    Name = "ppc",
-                    // Setting an in correct permit number, but it shouldn't matter as it's a waste exemption permit type we don't need a permit number.
-                    SelectedAuthorisationText = "EPR/"
-                }
-            ]
-        };
-
-        var validationResults = new List<ValidationResult>();
-
-        // Act
-        var result = Validator.TryValidateObject(model, new ValidationContext(model), validationResults);
-
-        // Assert
-        result.Should().BeTrue();
-        validationResults.Should().BeEmpty();
-    }
-
-    [TestMethod]
-    public void SelectAuthorisationTypeViewModel_ValidModel_SelectedAuthorisationPopulatedAndTextPopulatedWithCorrectFormat()
-    {
-        // Arrange
-        var model = new SelectAuthorisationTypeViewModel
-        {
-            SelectedAuthorisation = 2,
-            AuthorisationTypes =
-            [
-                new()
-                {
-                    Id = 2,
-                    Label = "label",
-                    Name = "ppc",
-                    SelectedAuthorisationText = "EPR/AB1234CD"
-                }
-            ]
-        };
-
-        var validationResults = new List<ValidationResult>();
-
-        // Act
-        var result = Validator.TryValidateObject(model, new ValidationContext(model), validationResults);
-
-        // Assert
-        result.Should().BeTrue();
-        validationResults.Should().BeEmpty();
-    }
-
-    [TestMethod]
-    public void SelectAuthorisationTypeViewModel_InvalidModel_WrongPermitNumberFormat()
-    {
-        // Arrange
-        var model = new SelectAuthorisationTypeViewModel
-        {
-            SelectedAuthorisation = 3,
-            AuthorisationTypes =
-            [
-                new()
-                {
-                    Id = 3,
-                    Label = "label",
-                    Name = "ppc",
-                    SelectedAuthorisationText = "EPR/"
-                }
-            ]
+            SelectedAuthorisation = null
         };
 
         var validationResults = new List<ValidationResult>();
@@ -88,6 +19,28 @@ public class SelectAuthorisationTypeViewModelUnitTests
 
         // Assert
         result.Should().BeFalse();
-        validationResults.Should().HaveCount(1);
+        validationResults.Should().BeEquivalentTo(new List<ValidationResult>
+        {
+            new("Select the type of permit the site has for accepting and recycling this waste", new List<string>{"SelectedAuthorisation"})
+        });
+    }
+
+    [TestMethod]
+    public void SelectAuthorisationTypeViewModel_ValidModel_RequiredFieldProvided()
+    {
+        // Arrange
+        var model = new SelectAuthorisationTypeViewModel
+        {
+            SelectedAuthorisation = 1
+        };
+
+        var validationResults = new List<ValidationResult>();
+
+        // Act
+        var result = Validator.TryValidateObject(model, new ValidationContext(model), validationResults);
+
+        // Assert
+        result.Should().BeTrue();
+        validationResults.Should().BeEmpty();
     }
 }
