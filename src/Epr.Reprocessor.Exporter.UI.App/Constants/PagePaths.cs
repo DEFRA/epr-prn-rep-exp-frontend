beab1555977cf1c3e5a280a68f171429be78eb3c
Bhaarath Balraj <bhaarath.balrak@Defra.onmicrosoft.com>
2025-06-30 10:38:01 +0100

    Merged changes with branch refreshed from feature/563047-ex-reg-overseas-reprocessor-details

--- src/Epr.Reprocessor.Exporter.UI.App/Constants/PagePaths.cs
+++ src/Epr.Reprocessor.Exporter.UI.App/Constants/PagePaths.cs
@@ -88,4 +88,8 @@ public class PagePaths
 
     // Use only for pages that have not been developed further than the current page being worked on.
     public const string Placeholder = "placeholder";
+    
+    public const string OverseasSiteDetails = "overseas-site-details";
+    public const string BaselConventionAndOECDCodes = "basel-OECD-codes";
+    public const string AddAnotherOverseasReprocessingSite = "add-another-overseas-reprocessing-site";
 }
