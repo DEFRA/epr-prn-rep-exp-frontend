2b72578fe1a949b783d39f6551dd854edd005fe5
Harikishnan Korasseril (ext) <harikishnan.korasseril.external@eviden.com>
2025-07-02 17:59:26 +0100

    Adding UI validation

--- src/Epr.Reprocessor.Exporter.UI/ViewModels/TypeOfSuppliersViewModel.cs
+++ src/Epr.Reprocessor.Exporter.UI/ViewModels/TypeOfSuppliersViewModel.cs
@@ -1,8 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Epr.Reprocessor.Exporter.UI.ViewModels;
 
 [ExcludeFromCodeCoverage]
 public class TypeOfSuppliersViewModel
 {
+    [Required(AllowEmptyStrings = false, ErrorMessage = "TypeOfSuppliers must be provided")]
+    [MaxLength(500, ErrorMessage = "TypeOfSuppliers must be 500 characters or less")]
     public string? TypeOfSuppliers { get; set; }
 
     public void MapForView(string typeOfSuppliers)
