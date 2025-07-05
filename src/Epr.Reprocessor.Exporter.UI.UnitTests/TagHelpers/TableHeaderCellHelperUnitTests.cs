bc0c7d25a857ec1b1bcbbc781df9cc3885da1312
hamzahshakeel907 <hamzah.shakeel.external@eviden.com>
2025-07-05 11:26:58 +0100

    added tag helpers and unit tests

---
+++ src/Epr.Reprocessor.Exporter.UI.UnitTests/TagHelpers/TableHeaderCellHelperUnitTests.cs
@@ -0,0 +1,24 @@
+﻿namespace Epr.Reprocessor.Exporter.UI.UnitTests.TagHelpers;
+
+[TestClass]
+public class TableHeaderCellHelperUnitTests : TagHelpersUnitTestBase
+{
+    [TestMethod]
+    public void Process_EnsureCorrectHtmlWithGdsStyling()
+    {
+        // Arrange
+        var tagHelper = new TableHeaderCellTagHelper();
+
+        var tagHelperContext = GenerateTagHelperContext(TableHeaderCellTagHelper.TagName);
+        var tagHelperOutput = GenerateTagHelperOutput(TableHeaderCellTagHelper.TagName, new TagHelperAttributeList());
+
+        // Act
+        tagHelper.Process(tagHelperContext, tagHelperOutput);
+
+        // Assert
+        var writer = new StringWriter();
+        tagHelperOutput.WriteTo(writer, HtmlEncoder.Default);
+
+        writer.ToString().Should().BeEquivalentTo("<th class=\"govuk-table__header\" scope=\"col\"></th>");
+    }
+}
---
+++ src/Epr.Reprocessor.Exporter.UI/TagHelpers/TaskListStatusTagTagHelper.cs
@@ -0,0 +1,30 @@
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace Epr.Reprocessor.Exporter.UI.TagHelpers;
+
+[HtmlTargetElement(TagName, Attributes = StatusAttribute)]
+public class TaskListStatusTagTagHelper : TagHelper
+{
+    private const string BaseCssClass = "govuk-tag";
+    private const string StatusAttribute = "asp-task-status";
+    private const string TagName = "task-list-status";
+
+    [HtmlAttributeName("asp-task-status")]
+    public TaskStatus Status { get; set; }
+
+    public override void Process(TagHelperContext context, TagHelperOutput output)
+    {
+        output.TagName = "strong";
+        
+        var additionalClass = Status switch
+        {
+            TaskStatus.CannotStartYet => "govuk-tag--grey",
+            TaskStatus.NotStart => "govuk-tag--grey",
+            TaskStatus.InProgress => "govuk-tag--blue",
+            TaskStatus.Completed => "govuk-tag--green",
+            _ => throw new ArgumentOutOfRangeException()
+        };
+
+        output.Attributes.Add("class", $"{BaseCssClass} {additionalClass}");
+    }
+}
