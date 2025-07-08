c7340d8bb64199f16f98c81debb18cbf9ba27d01
To-Sanh Nim <to-sanh.nim@atos.net>
2025-07-08 21:05:07 +0100

    initial code checkin for addressfornotices

---
+++ src/Epr.Reprocessor.Exporter.UI.UnitTests/Controllers/ExporterJourney/AddressForNoticesControllerUnitTests.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Address = Epr.Reprocessor.Exporter.UI.App.Domain.Address;
+using TaskStatus = Epr.Reprocessor.Exporter.UI.App.Enums.TaskStatus;
+
+namespace Epr.Reprocessor.Exporter.UI.UnitTests.Controllers.ExporterJourney;
+
+public class AddressForNoticesControllerUnitTests
+{
+}
---
+++ src/Epr.Reprocessor.Exporter.UI/Controllers/ExporterJourney/AddressForNoticesController.cs
@@ -0,0 +1,105 @@
+﻿using AutoMapper;
+using Epr.Reprocessor.Exporter.UI.Mapper;
+
+namespace Epr.Reprocessor.Exporter.UI.Controllers.ExporterJourney;
+
+[Route(PagePaths.ExporterAddressForNotice)]
+public class AddressForNoticesController(
+    ILogger<AddressForNoticesController> logger,
+    ISaveAndContinueService saveAndContinueService,
+    ISessionManager<ExporterRegistrationSession> sessionManager,
+    IValidationService validationService,
+    IMapper mapper,
+    IReprocessorService reprocessorService,
+    IRequestMapper requestMapper) : BaseExporterController<AddressForNoticesController>(logger, saveAndContinueService, sessionManager, mapper)
+{
+    private const string CurrentPage = PagePaths.AddressForNotices;
+    private const string NextPage = PagePaths.ExporterAddressForNotice;
+    private const string ViewPath = "~/Views/ExporterJourney/AddressForServiceOfNotice/AddressForNotices.cshtml";
+
+    private IValidationService ValidationService { get; } = validationService;
+    private IReprocessorService ReprocessorService { get; } = reprocessorService;
+    private IRequestMapper RequestMapper { get; } = requestMapper;
+
+    [HttpGet]
+    public async Task<IActionResult> Get()
+    {
+        await InitialiseSession();
+        var session = Session;
+        var reprocessingSite = session.RegistrationApplicationSession.ReprocessingSite;
+        session.Journey =
+        [
+            reprocessingSite!.ServiceOfNotice!.SourcePage ?? PagePaths.ExporterTaskList,
+            CurrentPage
+        ];
+
+        SetBackLink(CurrentPage);
+
+        var organisation = HttpContext.GetUserData().Organisations.FirstOrDefault()
+            ?? throw new ArgumentNullException("organisation");
+
+        var model = new AddressForNoticesViewModel
+        {
+            SelectedAddressOptions = reprocessingSite.TypeOfAddress,
+            IsBusinessAddress = string.IsNullOrEmpty(organisation.CompaniesHouseNumber),
+            BusinessAddress = new AddressViewModel
+            {
+                AddressLine1 = $"{organisation.BuildingNumber} {organisation.Street}",
+                AddressLine2 = organisation.Locality,
+                TownOrCity = organisation.Town ?? string.Empty,
+                County = organisation.County ?? string.Empty,
+                Postcode = organisation.Postcode ?? string.Empty
+            },
+            SiteAddress = new AddressViewModel
+            {
+                AddressLine1 = reprocessingSite.Address?.AddressLine1 ?? string.Empty,
+                AddressLine2 = reprocessingSite.Address?.AddressLine2 ?? string.Empty,
+                TownOrCity = reprocessingSite.Address?.Town ?? string.Empty,
+                County = reprocessingSite.Address?.County ?? string.Empty,
+                Postcode = reprocessingSite.Address?.Postcode ?? string.Empty
+            },
+            ShowSiteAddress = reprocessingSite.TypeOfAddress == AddressOptions.DifferentAddress
+        };
+
+        await SaveSession(CurrentPage);
+        return View(ViewPath, model);
+    }
+
+    [HttpPost]
+    [Route(PagePaths.AddressForNotices)]
+    public async Task<IActionResult> Post(AddressForNoticesViewModel model, string buttonAction)
+    {
+        await InitialiseSession();
+        var session = Session;
+        var reprocessingSite = session.RegistrationApplicationSession.ReprocessingSite;
+
+        session.Journey =
+        [
+            reprocessingSite!.ServiceOfNotice!.SourcePage ?? PagePaths.ExporterTaskList,
+            CurrentPage
+        ];
+
+        SetBackLink(CurrentPage);
+
+        var validationResult = await ValidationService.ValidateAsync(model);
+
+        if (!validationResult.IsValid)
+        {
+            ModelState.AddValidationErrors(validationResult);
+            return View(model);
+        }
+
+        if (model.SelectedAddressOptions is AddressOptions.DifferentAddress)
+        {
+            reprocessingSite.ServiceOfNotice!.TypeOfAddress = AddressOptions.DifferentAddress;
+        }
+        else
+        {
+            reprocessingSite.ServiceOfNotice!.SetAddress(model.GetAddress(), model.SelectedAddressOptions);
+        }
+
+        await SaveSession(CurrentPage);
+
+        return Redirect(model.SelectedAddressOptions is AddressOptions.DifferentAddress ? PagePaths.ExporterPostcodeForServiceOfNotices : PagePaths.ExporterRegistrationTaskList);
+    }
+}
