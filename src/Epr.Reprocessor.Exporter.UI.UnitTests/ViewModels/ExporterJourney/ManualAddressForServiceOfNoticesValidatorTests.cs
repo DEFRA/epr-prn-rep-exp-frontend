4cabef06c63df2cc18a4e45052bb654b7b5f0a21
ArunkumarNarayanaswamyEviden <ArunkumarNarayanaswamyEviden>
2025-07-08 17:00:43 +0100

    enter  service for notices address manually and saved address to session

---
+++ src/Epr.Reprocessor.Exporter.UI.UnitTests/ViewModels/ExporterJourney/ManualAddressForServiceOfNoticesValidatorTests.cs
@@ -0,0 +1,82 @@
+﻿
+using Epr.Reprocessor.Exporter.UI.Validations.ExporterJourney;
+using FluentValidation.TestHelper;
+using ManualAddressForServiceOfNoticesViewModel = Epr.Reprocessor.Exporter.UI.ViewModels.ExporterJourney.ManualAddressForServiceOfNoticesViewModel;
+
+namespace Epr.Reprocessor.Exporter.UI.UnitTests.Validations.ExporterJourney;
+
+[TestClass]
+public class ManualAddressForServiceOfNoticesValidatorTests
+{
+    private ManualAddressForServiceOfNoticesValidator _validator;
+
+    [TestInitialize]
+    public void Setup()
+    {
+        _validator = new ManualAddressForServiceOfNoticesValidator();
+    }
+
+    [TestMethod]
+    public void Validator_WithValidData_ShouldNotHaveErrors()
+    {
+        var model = new ManualAddressForServiceOfNoticesViewModel
+        {
+            AddressLine1 = "The Cupboard under the Stairs",
+            AddressLine2 = "4 Privet Drive",
+            TownOrCity = "Little Whinging",
+            County = "Surrey",
+            Postcode = "HP7 9PP"
+        };
+
+        var result = _validator.TestValidate(model);
+        result.ShouldNotHaveAnyValidationErrors();
+    }
+
+    [TestMethod]
+    public void Validator_WhenRequiredFieldsMissing_ShouldHaveErrors()
+    {
+        var model = new ManualAddressForServiceOfNoticesViewModel();
+
+        var result = _validator.TestValidate(model);
+
+        result.ShouldHaveValidationErrorFor(x => x.AddressLine1);
+        result.ShouldHaveValidationErrorFor(x => x.TownOrCity);
+        result.ShouldHaveValidationErrorFor(x => x.Postcode);
+    }
+
+    [TestMethod]
+    public void Validator_WhenPostcodeInvalid_ShouldHaveError()
+    {
+        var model = new ManualAddressForServiceOfNoticesViewModel
+        {
+            AddressLine1 = "1 Test",
+            TownOrCity = "City",
+            Postcode = "InvalidCode"
+        };
+
+        var result = _validator.TestValidate(model);
+
+        result.ShouldHaveValidationErrorFor(x => x.Postcode)
+              .WithErrorMessage("Enter a full UK postcode"); 
+    }
+
+    [TestMethod]
+    public void Validator_WhenFieldExceedsMaxLength_ShouldHaveError()
+    {
+        var model = new ManualAddressForServiceOfNoticesViewModel
+        {
+            AddressLine1 = new string('a', 101),
+            AddressLine2 = new string('b', 101),
+            TownOrCity = new string('c', 101),
+            County = new string('d', 101),
+            Postcode = "SW1A 1AA"
+        };
+
+        var result = _validator.TestValidate(model);
+
+        result.ShouldHaveValidationErrorFor(x => x.AddressLine1);
+        result.ShouldHaveValidationErrorFor(x => x.AddressLine2);
+        result.ShouldHaveValidationErrorFor(x => x.TownOrCity);
+        result.ShouldHaveValidationErrorFor(x => x.County);
+    }
+}
---
+++ src/Epr.Reprocessor.Exporter.UI/Controllers/ExporterJourney/ManualAddressForServiceOfNoticesController.cs
@@ -0,0 +1,81 @@
+﻿using AutoMapper;
+using Epr.Reprocessor.Exporter.UI.Controllers;
+using ManualAddressForServiceOfNoticesViewModel = Epr.Reprocessor.Exporter.UI.ViewModels.ExporterJourney.ManualAddressForServiceOfNoticesViewModel;
+
+[Route(PagePaths.ExporterManualAddressForServiceOfNotices)]
+[FeatureGate(FeatureFlags.ShowRegistration)]
+public class ManualAddressForServiceOfNoticesController(
+    ILogger<ManualAddressForServiceOfNoticesController> logger,
+    ISaveAndContinueService saveAndContinueService,
+    ISessionManager<ExporterRegistrationSession> sessionManager,
+    IValidationService validationService,
+    IMapper mapper)
+    : BaseExporterController<ManualAddressForServiceOfNoticesController>(logger, saveAndContinueService, sessionManager, mapper)
+{
+    private const string CurrentPage = PagePaths.ExporterManualAddressForServiceOfNotices;
+    private const string NextPage = PagePaths.ExporterCheckYourAnswersForNotices;
+    private const string ViewPath = "~/Views/ExporterJourney/AddressForServiceOfNotice/ManualAddressForNotices.cshtml";
+    private const string SaveAndContinueExporterPlaceholderKey = "ExporterManualAddressForNoticeSaveAndContinueKey";
+
+    [HttpGet]
+    public async Task<IActionResult> Get()
+    {
+        await InitialiseSession();
+        SetBackLink(CurrentPage);
+
+        var model = new ManualAddressForServiceOfNoticesViewModel();
+        var address = Session.LegalAddress;
+
+        if (address is not null)
+        {
+            model = new ManualAddressForServiceOfNoticesViewModel
+            {
+                AddressLine1 = address.AddressLine1,
+                AddressLine2 = address.AddressLine2,
+                County = address.County,
+                Postcode = address.PostCode,
+                TownOrCity = address.TownCity
+            };
+        }
+
+        return View(ViewPath, model);
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Post(ManualAddressForServiceOfNoticesViewModel model, string buttonAction)
+    {
+        await InitialiseSession();
+        SetBackLink(CurrentPage);
+
+        var validationResult = await validationService.ValidateAsync(model);
+        if (!validationResult.IsValid)
+        {
+            ModelState.AddValidationErrors(validationResult);
+            return View(ViewPath, model);
+        }
+
+        Session.LegalAddress = new AddressDto
+        {
+            AddressLine1 = model.AddressLine1,
+            AddressLine2 = model.AddressLine2,
+            TownCity = model.TownOrCity,
+            County = model.County,
+            PostCode = model.Postcode,
+            NationId = 1,
+            GridReference = string.Empty
+        };
+
+        await PersistJourneyAndSession(CurrentPage, NextPage, SaveAndContinueAreas.ExporterRegistration,
+            nameof(ManualAddressForServiceOfNoticesController), nameof(Get),
+            JsonConvert.SerializeObject(model), SaveAndContinueExporterPlaceholderKey);
+
+        return buttonAction switch
+        {
+            SaveAndContinueActionKey => Redirect(NextPage),
+            ConfirmAndContinueActionKey => Redirect(NextPage),
+            SaveAndComeBackLaterActionKey => Redirect(PagePaths.ExporterPlaceholder),
+            _ => View(ViewPath, model)
+        };
+    }
+}
