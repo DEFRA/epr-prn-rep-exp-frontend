d2f97ec52c94b1cc562e5eabd26861ddbd5a9a2a
To-Sanh Nim <to-sanh.nim@atos.net>
2025-07-10 12:43:56 +0100

    Validator

---
+++ src/Epr.Reprocessor.Exporter.UI/Validations/ExporterJourney/AddressForNoticesValidator.cs
@@ -0,0 +1,14 @@
+﻿using Epr.Reprocessor.Exporter.UI.Validations.Shared;
+using FluentValidation;
+using AddressForNoticesViewModel = Epr.Reprocessor.Exporter.UI.ViewModels.ExporterJourney.AddressForNoticesViewModel;
+
+namespace Epr.Reprocessor.Exporter.UI.Validations.ExporterJourney;
+
+public class AddressForNoticesValidator : AbstractValidator<AddressForNoticesViewModel>
+{
+    public AddressForNoticesValidator()
+    {
+        // Address
+        Include(new AddressValidator());
+    }
+}
---
+++ src/Epr.Reprocessor.Exporter.UI/ViewModels/ExporterJourney/AddressForNoticesViewModel.cs
@@ -0,0 +1,40 @@
+﻿namespace Epr.Reprocessor.Exporter.UI.ViewModels.ExporterJourney;
+
+[ExcludeFromCodeCoverage]
+public class AddressForNoticesViewModel : AddressViewModel
+{
+    #region Properties
+    public AddressOptions? SelectedAddressOptions { get; set; }
+
+    public AddressViewModel? BusinessAddress { get; set; }
+
+    public AddressViewModel? SiteAddress { get; set; }
+
+    public bool ShowSiteAddress { get; set; }
+
+    public bool IsBusinessAddress { get; set; }
+    #endregion
+
+    public Address? GetAddress() =>
+        SelectedAddressOptions switch
+        {
+            AddressOptions.RegisteredAddress => MapAddress(BusinessAddress),
+            AddressOptions.BusinessAddress => MapAddress(BusinessAddress),
+            AddressOptions.SiteAddress => MapAddress(SiteAddress),
+            AddressOptions.DifferentAddress => MapAddress(SiteAddress)
+        };
+
+    #region Mapping methods
+
+    private static Address? MapAddress(AddressViewModel? addressToMap)
+    {
+        if (addressToMap is null)
+        {
+            return null;
+        }
+
+        return new(addressToMap.AddressLine1, addressToMap.AddressLine2, null, addressToMap.TownOrCity,
+            addressToMap.County, null, addressToMap.Postcode);
+    }
+    #endregion
+}
