7bb96330eba499dd46bbbdde0689beb27d8dfc84
Jayakrishnan Appusamy <jayakrishnan.appusamy@atos.net>
2025-07-10 12:25:17 +0100

    Added required session classes to share across services

---
+++ src/Epr.Reprocessor.Exporter.UI/Sessions/RemoveUserJourneyModel.cs
@@ -0,0 +1,13 @@
+﻿namespace Epr.Reprocessor.Exporter.UI.Sessions;
+
+[ExcludeFromCodeCoverage]
+public class RemoveUserJourneyModel
+{
+	public string FirstName { get; set; }
+
+	public string LastName { get; set; }
+
+	public Guid PersonId { get; set; }
+
+	public string ServiceRoleKey { get; set; }
+}
