bd7b1773e70065b869b55a85c4f8db6a8067fca0
karimi-eviden <karim.imadanyen@defradev.onmicrosoft.com>
2025-07-14 09:29:37 +0100

    #582997 routing fixes for bug

--- src/Epr.Reprocessor.Exporter.UI/Controllers/AccreditationController.cs
+++ src/Epr.Reprocessor.Exporter.UI/Controllers/AccreditationController.cs
@@ -576,7 +576,8 @@ namespace Epr.Reprocessor.Exporter.UI.Controllers
             }
         }
 
-        [HttpGet(PagePaths.AccreditationSamplingAndInspectionPlan, Name = RouteIds.AccreditationSamplingAndInspectionPlan)]
+        [HttpGet(PagePaths.AccreditationSamplingAndInspectionPlan, Name = RouteIds.AccreditationSamplingAndInspectionPlan), 
+         HttpGet(PagePaths.AccreditationExporterSamplingAndInspectionPlan, Name = RouteIds.ExporterSamplingAndInspectionPlan)]
         public async Task<IActionResult> SamplingAndInspectionPlan(Guid accreditationId, Guid? submissionId = null)
         {
             // Get accreditation object
@@ -1257,7 +1258,8 @@ namespace Epr.Reprocessor.Exporter.UI.Controllers
                     RouteIds.MoreDetailOnBusinessPlanPERNs,
                     RouteIds.CheckBusinessPlanPERN,
                     RouteIds.SelectPernTonnage,
-                    RouteIds.ExporterAccreditationTaskList
+                    RouteIds.ExporterAccreditationTaskList,
+                    RouteIds.ExporterSamplingAndInspectionPlan
 
                 ];
         private void ValidateRouteForApplicationType(ApplicationType applicationType)
