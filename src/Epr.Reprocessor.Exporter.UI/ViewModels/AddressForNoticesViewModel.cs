d410ae720820985d8d359bd131c7b5d0dd091a8e
hamzahshakeel907 <hamzah.shakeel.external@eviden.com>
2025-07-15 09:00:12 +0100

    slight change

--- src/Epr.Reprocessor.Exporter.UI/ViewModels/AddressForNoticesViewModel.cs
+++ src/Epr.Reprocessor.Exporter.UI/ViewModels/AddressForNoticesViewModel.cs
@@ -42,9 +42,7 @@ public class AddressForNoticesViewModel
             AddressOptions.BusinessAddress => MapAddress(BusinessAddress),
             AddressOptions.SiteAddress => MapAddress(SiteAddress),
             AddressOptions.DifferentAddress => MapAddress(SiteAddress),
-            AddressOptions.None => null,
-            null => null,
-            _ => throw new ArgumentOutOfRangeException($"Value not in range")
+            _=> null
         };
 
     #region Mapping methods
