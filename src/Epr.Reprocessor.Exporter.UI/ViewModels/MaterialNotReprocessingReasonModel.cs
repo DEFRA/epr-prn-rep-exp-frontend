43f7d7d6f998820087fe9380b5764ef583449821
Harikishnan Korasseril (ext) <harikishnan.korasseril.external@eviden.com>
2025-07-17 11:30:18 +0100

    Correcting the label Adding warning

--- src/Epr.Reprocessor.Exporter.UI/ViewModels/MaterialNotReprocessingReasonModel.cs
+++ src/Epr.Reprocessor.Exporter.UI/ViewModels/MaterialNotReprocessingReasonModel.cs
@@ -7,7 +7,7 @@ public class MaterialNotReprocessingReasonModel
 {
     public Guid MaterialId { get; set; }
 
-    [Required(AllowEmptyStrings = false, ErrorMessage = "Enter the reason you’ll not reprocess this waste")]
+    [Required(AllowEmptyStrings = false, ErrorMessage = "Enter the reason you'll not reprocess this waste")]
     [MaxLength(500, ErrorMessage = "The list must be 500 characters or less")]
     public string? MaterialNotReprocessingReason { get; set; }
 
