b4f769e2ec55ad65456a5af86643ec7c25da2d9e
Masood Afzal <masood.afzal.external@eviden.com>
2025-09-04 11:19:12 +0100

    Fix SQ issues

--- src/Epr.Reprocessor.Exporter.UI.App/Constants/ValidationRegExConstants.cs
+++ src/Epr.Reprocessor.Exporter.UI.App/Constants/ValidationRegExConstants.cs
@@ -2,7 +2,7 @@
 
 namespace Epr.Reprocessor.Exporter.UI.App.Constants
 {
-    public class ValidationRegExConstants
+    public static class ValidationRegExConstants
     {
         public const string GridReferenceNoSpecialCharacters = @"^(?=.*\d)([a-zA-Z\d]*[\W_]?)*(?<![\W_])$";
         public const string GridReference10DigitMax = @"^\d{0,10}$";
