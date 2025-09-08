9612df2b694c584cbe0e94b5104d985bd1b222c0
Masood Afzal <masood.afzal.external@eviden.com>
2025-09-08 13:17:24 +0100

    Minor changes

--- src/Epr.Reprocessor.Exporter.UI.App/Services/Interfaces/IExporterRegistrationService.cs
+++ src/Epr.Reprocessor.Exporter.UI.App/Services/Interfaces/IExporterRegistrationService.cs
@@ -6,6 +6,6 @@ namespace Epr.Reprocessor.Exporter.UI.App.Services.Interfaces;
 public interface IExporterRegistrationService
 {
     Task SaveOverseasReprocessorAsync(OverseasAddressRequestDto request);
-    Task<List<OverseasMaterialReprocessingSiteDto>?> GetOverseasMaterialReprocessingSites(Guid RegistrationMaterialId);
+    Task<List<OverseasMaterialReprocessingSiteDto>?> GetOverseasMaterialReprocessingSites(Guid registrationMaterialId);
     Task UpsertInterimSitesAsync(SaveInterimSitesRequestDto request);
 }
